b5e6d0c22234ba3bd8d7dad9ecd3ddd5253e8538
SalvatoreMisia <104531745+SalvatoreMisia@users.noreply.github.com>
2022-06-29 13:41:15 +0200

    Update PostUserE_Step3_mEvaluation.m

--- APP/MODULES/PostUserE_Step3_mEvaluation.m
+++ APP/MODULES/PostUserE_Step3_mEvaluation.m
@@ -1,77 +1,82 @@
 %% Data import
 tic
 cd(fold_var)
-% Fig=uifigure;
-ProgressBar=uiprogressdlg(Fig,'Title','Please wait..','Message','Loading files','Cancelable','on','Indeterminate','on');
+
+% Fig = uifigure;
+ProgressBar = uiprogressdlg(Fig, 'Title','Please wait..', 'Message','Loading files', ...
+                                 'Cancelable','on', 'Indeterminate','on');
 drawnow
+
 load('GridCoordinates.mat')
-load('SoilParameters.mat');
-load('VegetationParameters.mat');
-load('GeneralRainfall.mat');
-load('RainInterpolated.mat');
-load('AnalysisInformation.mat');
-load('UserE_Answers.mat');
+load('SoilParameters.mat', 'KtAll')
+load('GeneralRainfall.mat')
+load('RainInterpolated.mat')
+load('AnalysisInformation.mat')
+load('UserE_Answers.mat')
 
 %% Preliminary operations and data extraction
-AnalysisNumber=StabilityAnalysis{1};
-RainStart=StabilityAnalysis{3}(1);
-RW=30*24;
-dt=(RW:-1:1);
+AnalysisNumber = StabilityAnalysis{1};
+RainStart = StabilityAnalysis{3}(1);
+RW = 30*24;
+dt = (RW:-1:1);
 
-if AnswerRainfallFor==1
-    HoursPrediction=cellfun(@max,SelectedHoursRun(:,1));
+if AnswerRainfallFor == 1
+    HoursPrediction = cellfun(@max,SelectedHoursRun(:,1));
 end
+
+KtStudyArea = cellfun(@(x,y) x(y), KtAll, ...
+                                   IndexDTMPointsInsideStudyArea, ...
+                                   'UniformOutput',false);
 
 %% Evalutation of m
-DmCumAll=cell(AnalysisNumber,length(xLongAll));
-Steps=AnalysisNumber*length(xLongAll)*RW;
-ProgressBar.Indeterminate='off';
-for i1=1:AnalysisNumber
-    Rain=cell(RW,1);
+DmCumPar = cell(AnalysisNumber,length(xLongAll));
+Steps = AnalysisNumber*length(xLongAll);
+ProgressBar.Indeterminate = 'off';
 
-    if AnswerRainfallFor==1
-        RunForecast=SelectedHoursRun{i1,2};
+IndexRainAnalysis = arrayfun(@(x) x:x+RW-1, 1:StabilityAnalysis{1}, 'UniformOutput',false);
+Rain = cell(1,AnalysisNumber);
+
+for i1 = 1:AnalysisNumber
+
+    ProgressBar.Message = strcat("Processing analysis event n. ",num2str(i1), ...
+                                 " of ",num2str(AnalysisNumber));
+    if ProgressBar.CancelRequested
+        break
+    end
+    
+    if AnswerRainfallFor == 1
+        RunForecast = SelectedHoursRun{i1,2};
         cd(fold_var_rain_for)
         load(strcat('RainForecastInterpolated',num2str(RunForecast)));
-        RainForecast=RainForecastInterpolated(SelectedHoursRun{i1,1},:);
+        RainForecast = RainForecastInterpolated(SelectedHoursRun{i1,1},:);
     end
 
-    for i2=1:length(xLongAll)
-        RowNumber=size(xLongAll{i2},1);
-        ColumnNumber=size(xLongAll{i2},2);
-        DmCumPar=zeros(RowNumber,ColumnNumber);
-        %Rain=RainInterpolated(RainStart+i1-1:RW-1+RainStart+i1-1,i2);
-        Rain=RainInterpolated(i1:RW-1+i1,i2);
-        %Rain(1:RW-HoursPrediction(i1))=RainInterpolated(i1:RW-1+i1-HoursPrediction(i1),i2);
+    for i2 = 1:length(xLongAll)
+        RowNumber = size(xLongAll{i2},1);
+        ColumnNumber = size(xLongAll{i2},2);
 
-        if AnswerRainfallFor==1
-            Rain(RW-HoursPrediction(i1)+1:RW)=RainForecast(:,1);
+        ExpKtDt = arrayfun(@(x) exp(-KtStudyArea{i2}*x), dt, 'UniformOutput',false);
+        Rain = cellfun(@(x) RainInterpolated(x,i2), IndexRainAnalysis, 'UniformOutput', false);
+
+        if AnswerRainfallFor == 1
+            Rain{i1}(RW-HoursPrediction(i1)+1:RW) = RainForecast(:,1); % MG had put i2 instead of 1, please investigate
         end
+
+        DmCumTemp = cellfun(@(x,y) full(x)./1000.*y, Rain{i1}, ...
+                                                     ExpKtDt', ...
+                                                     'UniformOutput',false);
+
+        DmCumPar{i1,i2} = sum( cat(3, DmCumTemp{:}), 3 );
         
-        for i3=1:RW
-            %ProgressBar.Value=((i1*i2-1)*RW+i3)/Steps;
-            ProgressBar.Value=(length(xLongAll)*RW*(i1-1)+RW*(i2-1)+i3)/Steps;
-            if ProgressBar.CancelRequested
-                break
-            end
-            Hw=Rain{i3}./1000; % It is important to have Rain in mm
-            if any(Hw)==1
-                DmCumPar(IndexDTMPointsInsideStudyArea{i2})=DmCumPar(IndexDTMPointsInsideStudyArea{i2})+...
-                            Hw.*exp(-KtAll{i2}(IndexDTMPointsInsideStudyArea{i2}).*dt(i3));
-            end
-            ProgressBar.Message=strcat("Processing analysis event n. ",num2str(i1)," of ",num2str(AnalysisNumber));
-        end
-
-        DmCum{i1,i2}=min(BetaStarAll{i2}(IndexDTMPointsInsideStudyArea{i2})./...
-                        (nAll{i2}(IndexDTMPointsInsideStudyArea{i2}).*H.*...
-                        (1-Sr0)).*DmCumPar(IndexDTMPointsInsideStudyArea{i2}), 1); % You cannot have DmCum > 1 because when you have 1 is already completely saturated
+        ProgressBar.Value = (length(xLongAll)*(i1-1)+i2)/Steps;
     end
-    %disp(strcat('Finished n. ',num2str(i1)," of ",num2str(AnalysisNumber)))
+
 end
-close(ProgressBar)
+
+close(ProgressBar) % ProgressBar if in app version
 toc
 
-%% Saving..
+%% Saving...
 cd(fold_var)
-save('DmCum.mat','DmCum','-v7.3');
+save('DmCum.mat', 'DmCumPar', '-v7.3')
 cd(fold0)
