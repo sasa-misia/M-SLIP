d24fb85ee02572c5e63bb546fe90cbe766ec724a
SalvatoreMisia <104531745+SalvatoreMisia@users.noreply.github.com>
2022-06-29 13:40:14 +0200

    Update PostUserD_Vegetation_ParameterAttribution1_Step1LU.m

--- APP/MODULES/PostUserD_Vegetation_ParameterAttribution1_Step1LU.m
+++ APP/MODULES/PostUserD_Vegetation_ParameterAttribution1_Step1LU.m
@@ -10,34 +10,34 @@ if exist('IndexLandUsesToRemove')
     AllLandUnique=AllLandUnique(AllIndex);
 end
 
-VegPolygonsStudyArea=LandUsePolygonsStudyArea;
-VegetationAllUnique=AllLandUnique;
-VegFieldName=LandUsesFieldName;
+VegPolygonsStudyArea = LandUsePolygonsStudyArea;
+VegetationAllUnique = AllLandUnique;
+VegFieldName = LandUsesFieldName;
 
 %% Writing of an excel that User has to compile before Step2
 cd(fold_user)
-ColHeader1={'UV','c_R''(kPa)','\beta (-)','Color'};
-ColHeader2={LandUsesFieldName,'UV associated','VU Abbrev (For Map)','RGB VU (for Map)',};
+ColHeader1 = {'UV', 'c_R''(kPa)', '\beta (-)', 'Color'};
+ColHeader2 = {LandUsesFieldName,'UV associated','VU Abbrev (For Map)','RGB VU (for Map)',};
 
-writecell(ColHeader1,'VUDVCAssociation.xlsx','Sheet','DVCParameters','Range','A1');
-writecell(ColHeader2,'VUDVCAssociation.xlsx','Sheet','Association','Range','A1');
-writecell(AllLandUnique','VUDVCAssociation.xlsx','Sheet','Association','Range','A2');
+writecell(ColHeader1,'VUDVCAssociation.xlsx', 'Sheet','DVCParameters', 'Range','A1');
+writecell(ColHeader2,'VUDVCAssociation.xlsx', 'Sheet','Association', 'Range','A1');
+writecell(AllLandUnique','VUDVCAssociation.xlsx', 'Sheet','Association', 'Range','A2');
 
 FileName_VegAssociation='VuDVCAssociation.xlsx';
 if isfile(FileName_VegAssociation)
     warning(strcat(FileName_VegAssociation,' already exist'))
 end
-ColHeader1={VegFieldName,'UV associated','VU Abbrev (For Map)','RGB LU (for Map)'};
-ColHeader2={'UV','c_R''(kPa)','\beta (-)','Color'};
-writecell(ColHeader1,FileName_VegAssociation,'Sheet','Association','Range','A1');
-writecell(VegetationAllUnique',FileName_VegAssociation,'Sheet','Association','Range','A2');
-writecell(ColHeader2,FileName_VegAssociation,'Sheet','DVCParameters','Range','A1');
+ColHeader1 = {VegFieldName, 'UV associated', 'VU Abbrev (For Map)', 'RGB LU (for Map)'};
+ColHeader2 = {'UV', 'c_R''(kPa)', '\beta (-)', 'Color'};
+writecell(ColHeader1,FileName_VegAssociation, 'Sheet','Association', 'Range','A1');
+writecell(VegetationAllUnique',FileName_VegAssociation, 'Sheet','Association', 'Range','A2');
+writecell(ColHeader2,FileName_VegAssociation, 'Sheet','DVCParameters', 'Range','A1');
 
-VariablesVeg = {'VegPolygonsStudyArea','VegetationAllUnique','FileName_VegAssociation'};
-VariablesAnswer = {'AnswerAttributionVegetationParameter';'FileName_Vegetation';'VegFieldName'};
+VariablesVeg = {'VegPolygonsStudyArea', 'VegetationAllUnique', 'FileName_VegAssociation'};
+VariablesAnswerD = {'AnswerAttributionVegetationParameter', 'FileName_Vegetation', 'VegFieldName'};
 
 %% Saving of polygons included in the study area
 cd(fold_var)
-save('VegPolygonsStudyArea.mat',VariablesVeg{:});
-save('UserD_Answers.mat',VariablesAnswer{:});
+save('VegPolygonsStudyArea.mat', VariablesVeg{:});
+save('UserD_Answers.mat', VariablesAnswerD{:}, '-append');
 cd(fold0)
