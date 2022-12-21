a21bad821201f7e9d7c8c386a458ed8657afcdb0
SalvatoreMisia <104531745+SalvatoreMisia@users.noreply.github.com>
2022-12-21 13:33:34 +0100

    Update PlotMorphology.m

--- APP/MODULES/PlotMorphology.m
+++ APP/MODULES/PlotMorphology.m
@@ -1,7 +1,8 @@
+%% File loading
 cd(fold_var)
-load('StudyAreaVariables.mat');
+load('StudyAreaVariables.mat')
 load('GridCoordinates.mat')
-load('MorphologyParameters.mat');
+load('MorphologyParameters.mat')
 
 if exist('PlotSettings.mat', 'file')
     load('PlotSettings.mat')
@@ -13,273 +14,285 @@ else
     LegendPosition = 'Best';
 end
 
+InfoDetectedExist = false;
+if exist('InfoDetectedSoilSlips.mat', 'file')
+    load('InfoDetectedSoilSlips.mat', 'InfoDetectedSoilSlips')
+    InfoDetectedExist = true;
+end
+
 %% For scatter dimension
-RefStudyArea=0.0417;
-ExtentStudyArea=area(StudyAreaPolygon);
-RatioRef=ExtentStudyArea/RefStudyArea;
+RefStudyArea = 0.0417;
+ExtentStudyArea = area(StudyAreaPolygon);
+% ExtentStudyArea = prod(MaxExtremes-MinExtremes);
+RatioRef = ExtentStudyArea/RefStudyArea;
+PixelSize = .05/RatioRef;
+DetPixelSize = 2*PixelSize;
 
-DimFigure=[8.5 6.5];
+%% Data extraction
+xLongStudy = cellfun(@(x,y) x(y), xLongAll, IndexDTMPointsInsideStudyArea, 'UniformOutput',false);
+clear('xLongAll')
 
+yLatStudy = cellfun(@(x,y) x(y), yLatAll, IndexDTMPointsInsideStudyArea, 'UniformOutput',false);
+clear('yLatAll')
 
-%% Loading
-% NumFigPlot=1; %1 Elevation 2 Slope 3 Aspect
-slope_range=10:10:60;
-aspect_range=90:90:360;
-altitude_range=[200 250 300 350 400 450 500  600  700 800 900];
+ElevationStudy = cellfun(@(x,y) x(y), ElevationAll, IndexDTMPointsInsideStudyArea, 'UniformOutput',false);
+clear('ElevationAll')
 
-xLongStudy=cell(size(xLongAll));
-yLatStudy=cell(size(xLongAll));
-SlopeStudy=cell(size(xLongAll));
-AspectStudy=cell(size(xLongAll));
-GradEStudy=cell(size(xLongAll));
-GradNStudy=cell(size(xLongAll));
-ElevationStudy=cell(size(xLongAll));
+SlopeStudy = cellfun(@(x,y) x(y), SlopeAll, IndexDTMPointsInsideStudyArea, 'UniformOutput',false);
+clear('SlopeAll')
 
-ColorElevation=[103 181 170;
-    127 195 186;
-    152 210 199;
-    177 225 217;
-    200 232 226;
-    225 240 238;
-    245 237 224;
-    240 227 200;
-    235 217 176;
-    223 198 157;
-    213 179 136;
-    201 159 116];
+AspectStudy = cellfun(@(x,y) x(y), AspectAngleAll, IndexDTMPointsInsideStudyArea, 'UniformOutput',false);
+clear('AspectAngleAll')
+
+GradEStudy = cellfun(@(x,y) x(y), GradEAll, IndexDTMPointsInsideStudyArea, 'UniformOutput',false);
+clear('GradEAll')
+
+GradNStudy = cellfun(@(x,y) x(y), GradNAll, IndexDTMPointsInsideStudyArea, 'UniformOutput',false);
+clear('GradNAll')
+
+%% Ranges
+AltMin = min(cellfun(@min, ElevationStudy));
+AltMax = max(cellfun(@max, ElevationStudy));
+AltRange = linspace(AltMin, AltMax, 11)';
+% AltRange = [200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900];
+
+if AltMax <= 1000
+    ValuesAlt = round(AltRange, 2, 'significant');
+else
+    ValuesAlt = round(AltRange, 3, 'significant');
+end
+
+LegendCaptionAlt = [strcat(string(ValuesAlt(1:end-1)), " - ", string(ValuesAlt(2:end)))];
+
+SlopeRange = (0:10:60)';
+LegendCaptionSlope = [strcat(string(SlopeRange(1:end-1)), " - ", string(SlopeRange(2:end))); strcat("> ",string(SlopeRange(end)))];
+
+AspectRange = (0:90:360)';
+LegendCaptionAspect = [strcat(string(AspectRange(1:end-1)), " - ", string(AspectRange(2:end)))];
+
+ColorElevation = [ 103 181 170
+                   127 195 186
+                   152 210 199
+                   177 225 217
+                   200 232 226
+                   225 240 238
+                   245 237 224
+                   240 227 200
+                   235 217 176
+                   223 198 157
+                   213 179 136
+                   201 159 116 ];
     
+% ColorSlope = [ 201 160 220
+%                143 000 255
+%                000 100 025
+%                127 255 212
+%                000 187 045
+%                255 255 102
+%                255 153 000 ];
 
-ColorSlope=[201 160 220;
-    143 0 255;
-    0 100 25;
-    127 255 212;
-    0 187 45;
-    255 255 102;
-    255 153 0];
+ColorSlope = cool(7)*255;
 
-ColorAspect=[201 160 220;
-    143 0 255;
-    0 100 255;
-    127 255 212];
+ColorAspect = [ 201 160 220
+                143 000 255
+                000 100 255
+                127 255 212 ];
 
-%%
-xLongStudy=cellfun(@(x,y) x(y),xLongAll,IndexDTMPointsInsideStudyArea,...
-        'UniformOutput',false);
-
-yLatStudy=cellfun(@(x,y) x(y),yLatAll,IndexDTMPointsInsideStudyArea,...
-        'UniformOutput',false);
-
-ElevationStudy=cellfun(@(x,y) x(y),ElevationAll,IndexDTMPointsInsideStudyArea,...
-        'UniformOutput',false);
-
-SlopeStudy=cellfun(@(x,y) x(y),SlopeAll,IndexDTMPointsInsideStudyArea,...
-        'UniformOutput',false);
-
-AspectStudy=cellfun(@(x,y) x(y),AspectAngleAll,IndexDTMPointsInsideStudyArea,...
-        'UniformOutput',false);
-
-GradEStudy=cellfun(@(x,y) x(y),GradEAll,IndexDTMPointsInsideStudyArea,...
-        'UniformOutput',false);
-
-GradNStudy=cellfun(@(x,y) x(y),GradNAll,IndexDTMPointsInsideStudyArea,...
-        'UniformOutput',false);
-
-for i1=1:length(slope_range)+1
-    if i1==1
-        SlopeIndex(i1,:)=cellfun(@(x) find(x<=slope_range(i1)),SlopeStudy,'UniformOutput',false);
-    elseif i1>1 & i1<length(slope_range)+1
-        SlopeIndex(i1,:)=cellfun(@(x) find(x>=slope_range(i1-1)+.01 & x<=slope_range(i1)),SlopeStudy,'UniformOutput',false);
+%% Creation of goups based on ranges
+ElevationIndex = cell(length(AltRange)-1, size(ElevationStudy,2));
+for i1 = 1:length(AltRange)-1
+    if i1 < length(AltRange)-1
+        ElevationIndex(i1,:) = cellfun(@(x) find(x>=AltRange(i1) & x<AltRange(i1+1)), ElevationStudy, 'UniformOutput',false);
     else
-        SlopeIndex(i1,:)=cellfun(@(x) find(x>=slope_range(i1-1)+.01),SlopeStudy,'UniformOutput',false);
+        ElevationIndex(i1,:) = cellfun(@(x) find(x>=AltRange(i1) & x<=AltRange(i1+1)), ElevationStudy, 'UniformOutput',false);
     end
 end
 
-
-for i2=1:length(aspect_range)
-    if i2==1
-        AspectIndex(i2,:)=cellfun(@(x) find(x<=aspect_range(i2)),AspectStudy,'UniformOutput',false);
+SlopeIndex = cell(length(SlopeRange), size(SlopeStudy,2));
+for i1 = 1:length(SlopeRange)
+    if i1 < length(SlopeRange)
+        SlopeIndex(i1,:) = cellfun(@(x) find(x>=SlopeRange(i1) & x<SlopeRange(i1+1)), SlopeStudy, 'UniformOutput',false);
     else
-        AspectIndex(i2,:)=cellfun(@(x) find(x>=aspect_range(i2-1)+.01 & x<=aspect_range(i2)),AspectStudy,'UniformOutput',false);
+        SlopeIndex(i1,:) = cellfun(@(x) find(x>=SlopeRange(i1)), SlopeStudy, 'UniformOutput',false);
     end
 end
 
-
-for i3=1:length(altitude_range)+1
-    if i3==1
-        ElevationIndex(i3,:)=cellfun(@(x) find(x<=altitude_range(i3)),ElevationStudy,'UniformOutput',false);
-    elseif i3>1 & i3<length(altitude_range)+1
-        ElevationIndex(i3,:)=cellfun(@(x) find(x>=altitude_range(i3-1)+.01 & x<=altitude_range(i3)),ElevationStudy,'UniformOutput',false);
+AspectIndex = cell(length(AspectRange)-1, size(AspectStudy,2));
+for i1 = 1:length(AspectRange)-1
+    if i1 < length(AspectRange)-1
+        AspectIndex(i1,:) = cellfun(@(x) find(x>=AspectRange(i1) & x<AspectRange(i1+1)), AspectStudy, 'UniformOutput',false);
     else
-        ElevationIndex(i3,:)=cellfun(@(x) find(x>=altitude_range(i3-1)+.01),ElevationStudy,'UniformOutput',false);
+        AspectIndex(i1,:) = cellfun(@(x) find(x>=AspectRange(i1) & x<=AspectRange(i1+1)), AspectStudy, 'UniformOutput',false);
     end
 end
 
-
-%%
+%% Plot based on user selection
 switch NumFigPlot
     case 1
-        filename1='Elevation_Range';
-        f1=figure(1);
-        set(f1 , ...
-            'Color',[1 1 1],...
-            'PaperType','a4',...
-            'PaperSize',[29.68 20.98 ],...    
-            'PaperUnits', 'centimeters',...
-            'PaperPositionMode','manual',...
-            'PaperPosition', [0 1 DimFigure(1) DimFigure(2)],...
-            'InvertHardcopy','off');
-        set( gcf ,'Name' , filename1);
+        %% Elevation
+        filename1 = 'Elevation_Range';
+        f1 = figure(1);
+        ax1 = axes('Parent',f1); 
+        hold(ax1,'on'); 
 
-        axes1 = axes('Parent',f1); 
-        hold(axes1,'on');  
+        set(gcf, 'Name',filename1); 
         
-        for i1=1:length(altitude_range)+1
-            helevation(i1,:)=cellfun(@(x,y,z) scatter(x(z),y(z),.1/RatioRef,...
-            'Marker','o','MarkerFaceColor',ColorElevation(i1,:)./255,'MarkerEdgeColor','none'),...
-            xLongStudy,yLatStudy,ElevationIndex(i1,:),'UniformOutput',false);
-            hold on
+        helevation = cell(length(AltRange)-1, size(xLongStudy,2));
+        for i1 = 1:length(AltRange)-1
+            helevation(i1,:) = cellfun(@(x,y,z) scatter(x(z),y(z), PixelSize, 'Marker','o', ...
+                                                        'MarkerFaceColor',ColorElevation(i1,:)./255, ...
+                                                        'MarkerEdgeColor','none'), ...
+                                        	xLongStudy, yLatStudy, ElevationIndex(i1,:), 'UniformOutput',false);
         end       
         
-        
-        plot(StudyAreaPolygon,'FaceColor','none','LineWidth',1.5)
-        hold on
+        plot(StudyAreaPolygon, 'FaceColor','none', 'LineWidth',1.5)
+
+        fig_settings(fold0)
+
+        if InfoDetectedExist
+            hdetected = cellfun(@(x,y) scatter(x, y, DetPixelSize, '^k','Filled'), InfoDetectedSoilSlips(:,5), InfoDetectedSoilSlips(:,6));
+            uistack(hdetected,'top')
+        end
 
         if exist('LegendPosition', 'var')
-            hleg3=legend([helevation{1:end,1}],...
-                '< 200', ...
-                '200 - 250', ...
-                '250 - 300', ...
-                '300 - 350', ...
-                '350 - 400', ...
-                '400 - 450', ...
-                '450 - 500', ...
-                '500 - 600', ...
-                '600 - 700', ...
-                '700 - 800', ...
-                '800 - 900', ...
-                '> 900', ...
-                'NumColumns',2, ...
-                'FontName',SelectedFont, ...
-                'FontSize',SelectedFontSize ...
-                'Location',LegendPosition);
+            LegendObjects = helevation(1:end,1);
+            LegendCaption = cellstr(LegendCaptionAlt);
+
+            if InfoDetectedExist
+                LegendObjects = [LegendObjects; {hdetected(1)}];
+                LegendCaption = [LegendCaption; {"Points Analyzed"}];
+            end
+
+            hleg3 = legend([LegendObjects{:}], ...
+                            LegendCaption, ...
+                           'NumColumns',2, ...
+                           'FontName',SelectedFont, ...
+                           'FontSize',SelectedFontSize, ...
+                           'Location',LegendPosition, ...
+                           'Box','off');
             
             legend('AutoUpdate','off');
-            legend boxoff
-            hleg3.ItemTokenSize(1)=10;
+            hleg3.ItemTokenSize(1) = 5;
             
             title(hleg3, 'Elevation [m]', 'FontName',SelectedFont, 'FontSize',SelectedFontSize*1.2, 'FontWeight','bold')
-        end
 
-        fig_settings(fold0)
-        
-        set(gca,'visible','off')
-        cd(fold_fig)
-        exportgraphics(f1,strcat(filename1,'.png'),'Resolution',600);
-
-    case 2
-        filename2='sub_slope';
-        f2=figure(2);
-        set(f2 , ...
-            'Color',[1 1 1],...
-            'PaperType','a4',...
-            'PaperSize',[29.68 20.98 ],...
-            'PaperUnits', 'centimeters',...
-            'PaperPositionMode','manual',...
-            'PaperPosition', [0 1 DimFigure(1) DimFigure(2)],...
-            'InvertHardcopy','off');
-        set( gcf ,'Name' , filename2);
-        
-        axes2 = axes('Parent',f2); 
-        hold(axes2,'on');
-        
-        for i1=1:length(slope_range)+1
-            hslope(i1,:)=cellfun(@(x,y,z) scatter(x(z),y(z),.1/RatioRef,...
-            'Marker','o','MarkerFaceColor',ColorSlope(i1,:)./255,'MarkerEdgeColor','none'),...
-            xLongStudy,yLatStudy,SlopeIndex(i1,:),'UniformOutput',false);
-            hold on
-        end
-
-        plot(StudyAreaPolygon,'FaceColor','none','LineWidth',1.5)
-        hold on
-        
-        if exist('LegendPosition', 'var')
-            hleg=legend([hslope{1:end,1}],...
-                '0 - 10',...
-                '10 - 20',...
-                '20 - 30',...
-                '30 - 40',...
-                '40 - 50',...
-                '50 - 60',...
-                '> 60',...
-                'NumColumns',2,...
-                'FontName',SelectedFont,...
-                'Location',LegendPosition,...
-                'FontSize',SelectedFontSize);
-            hleg.ItemTokenSize(1)=10;
-            legend('AutoUpdate','off');
-            legend boxoff
-    
-            title(hleg, 'Slope angle [°]', 'FontName',SelectedFont, 'FontSize',SelectedFontSize*1.2, 'FontWeight','bold')
-        end
-        
-        fig_settings(fold0)
-
-        set(gca,'visible','off')
-        cd(fold_fig)
-        exportgraphics(f2, strcat(filename2,'.png'), 'Resolution',600);
-
-%%
-    case 3
-        filename3='sub_aspect';
-        f3=figure(3);
-        set(f3 , ...
-            'Color',[1 1 1],...
-            'PaperType','a4',...
-            'PaperSize',[29.68 20.98 ],...    
-            'PaperUnits', 'centimeters',...
-            'PaperPositionMode','manual',...
-            'PaperPosition', [0 1 DimFigure(1) DimFigure(2)],...
-            'InvertHardcopy','off');
-        set( gcf ,'Name' , filename3);
-
-        axes3 = axes('Parent',f3); 
-        hold(axes3,'on');
-        
-        for i1=1:length(aspect_range)
-            haspect(i1,:)=cellfun(@(x,y,z) scatter(x(z),y(z),.1/RatioRef,...
-            'Marker','o','MarkerFaceColor',ColorAspect(i1,:)./255,'MarkerEdgeColor','none'),...
-            xLongStudy,yLatStudy,AspectIndex(i1,:),'UniformOutput',false);
-            hold on
-        end
-
-        
-        plot(StudyAreaPolygon,'FaceColor','none','LineWidth',1.5)
-        hold on
-        
-        if exist('LegendPosition', 'var')
-            hleg2=legend([haspect{1:end,1}],...
-                '0 - 90',...
-                '90 - 180',...
-                '180 - 270',...
-                '270 - 360',...
-                'NumColumns',2,...
-                'FontName',SelectedFont,...
-                'Location',LegendPosition,...
-                'FontSize',SelectedFontSize);
-            
-            legend('AutoUpdate','off');
-            
-            hleg2.ItemTokenSize(1)=10;
-    
-            fig_settings(fold0)
-            
-            title(hleg2, 'Aspect angle [°]', 'FontName',SelectedFont, 'FontSize',SelectedFontSize*1.2, 'FontWeight','bold')
-            
-            legend('AutoUpdate','off');
-            legend boxoff
+            fig_rescaler(f1, hleg3, LegendPosition)
         end
         
         set(gca, 'visible','off')
+
+        cd(fold_fig)
+        exportgraphics(f1, strcat(filename1,'.png'), 'Resolution',600);
+
+    case 2
+        %% Slope
+        filename2 = 'sub_slope';
+        f2 = figure(2);
+        ax2 = axes('Parent',f2); 
+        hold(ax2,'on');
+
+        set(gcf, 'Name',filename2);
+        
+        hslope = cell(length(SlopeRange), size(xLongStudy,2));
+        for i1 = 1:length(SlopeRange)
+            hslope(i1,:) = cellfun(@(x,y,z) scatter(x(z), y(z), PixelSize, 'Marker','o', ...
+                                                            'MarkerFaceColor',ColorSlope(i1,:)./255, ...
+                                                            'MarkerEdgeColor','none'), ...
+                                        xLongStudy, yLatStudy, SlopeIndex(i1,:), 'UniformOutput',false);
+        end
+
+        plot(StudyAreaPolygon, 'FaceColor','none', 'LineWidth',1.5)
+
+        fig_settings(fold0)
+
+        if InfoDetectedExist
+            hdetected = cellfun(@(x,y) scatter(x, y, DetPixelSize, '^k','Filled'), InfoDetectedSoilSlips(:,5), InfoDetectedSoilSlips(:,6));
+            uistack(hdetected,'top')
+        end
+        
+        if exist('LegendPosition', 'var')
+            LegendObjects = hslope(1:end,1);
+            LegendCaption = cellstr(LegendCaptionSlope);
+
+            if InfoDetectedExist
+                LegendObjects = [LegendObjects; {hdetected(1)}];
+                LegendCaption = [LegendCaption; {"Points Analyzed"}];
+            end
+
+            hleg = legend([LegendObjects{:}], ...
+                           LegendCaption, ...
+                          'NumColumns',2, ...
+                          'FontName',SelectedFont, ...
+                          'Location',LegendPosition, ...
+                          'FontSize',SelectedFontSize, ...
+                          'Box','off');
+
+            hleg.ItemTokenSize(1) = 5;
+            legend('AutoUpdate','off');
+    
+            title(hleg, 'Slope angle [°]', 'FontName',SelectedFont, 'FontSize',SelectedFontSize*1.2, 'FontWeight','bold')
+
+            fig_rescaler(f2, hleg, LegendPosition)
+        end
+
+        set(gca, 'visible','off')
+
+        cd(fold_fig)
+        exportgraphics(f2, strcat(filename2,'.png'), 'Resolution',600);
+
+    case 3 
+        %% Aspect
+        filename3 = 'sub_aspect';
+        f3 = figure(3);
+        ax3 = axes('Parent',f3); 
+        hold(ax3,'on');
+
+        set(gcf, 'Name',filename3);
+        
+        haspect = cell(length(AspectRange)-1, size(xLongStudy,2));
+        for i1 = 1:length(AspectRange)-1
+            haspect(i1,:) = cellfun(@(x,y,z) scatter(x(z), y(z), PixelSize, 'Marker','o', ...
+                                                        'MarkerFaceColor',ColorAspect(i1,:)./255, ...
+                                                        'MarkerEdgeColor','none'), ...
+                                        xLongStudy, yLatStudy, AspectIndex(i1,:), 'UniformOutput',false);
+        end
+        
+        plot(StudyAreaPolygon, 'FaceColor','none', 'LineWidth',1.5)
+
+        fig_settings(fold0)
+
+        if InfoDetectedExist
+            hdetected = cellfun(@(x,y) scatter(x, y, DetPixelSize, '^k','Filled'), InfoDetectedSoilSlips(:,5), InfoDetectedSoilSlips(:,6));
+            uistack(hdetected,'top')
+        end
+        
+        if exist('LegendPosition', 'var')
+            LegendObjects = haspect(1:end,1);
+            LegendCaption = cellstr(LegendCaptionAspect);
+
+            if InfoDetectedExist
+                LegendObjects = [LegendObjects; {hdetected(1)}];
+                LegendCaption = [LegendCaption; {"Points Analyzed"}];
+            end
+
+            hleg2 = legend([LegendObjects{:}], ...
+                            LegendCaption, ...
+                           'NumColumns',2, ...
+                           'FontName',SelectedFont, ...
+                           'Location',LegendPosition, ...
+                           'FontSize',SelectedFontSize, ...
+                           'Box','off');
+            
+            hleg2.ItemTokenSize(1) = 5;
+            legend('AutoUpdate','off');
+            
+            title(hleg2, 'Aspect angle [°]', 'FontName',SelectedFont, 'FontSize',SelectedFontSize*1.2, 'FontWeight','bold')
+
+            fig_rescaler(f3, hleg2, LegendPosition)
+        end
+        
+        set(gca, 'visible','off')
+
         cd(fold_fig)
         exportgraphics(f3, strcat(filename3,'.png'), 'Resolution',600);
 end
