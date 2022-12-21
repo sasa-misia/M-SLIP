3582422264a0e5d69da491970a350ead781041ef
SalvatoreMisia <104531745+SalvatoreMisia@users.noreply.github.com>
2022-12-21 13:33:31 +0100

    Update PlotLandUse.m

--- APP/MODULES/PlotLandUse.m
+++ APP/MODULES/PlotLandUse.m
@@ -1,154 +1,169 @@
+%% File loading
 cd(fold_var)
-load('LandUsesVariables.mat');
-load('StudyAreaVariables.mat');
+load('LandUsesVariables.mat')
+load('StudyAreaVariables.mat')
 
-OrthophotoAnswer=0;
-if exist("Orthophoto.mat")
-    load("Orthophoto.mat")
-    OrthophotoAnswer=1;
+OrthophotoAnswer = 0;
+if exist('Orthophoto.mat', 'file')
+    load('Orthophoto.mat')
+    OrthophotoAnswer = 1;
 end
 
-
-
-if exist('LegendSettings.mat')
-    load('LegendSettings.mat')
+if exist('PlotSettings.mat', 'file')
+    load('PlotSettings.mat')
+    SelectedFont = Font;
+    SelectedFontSize = FontSize;
 else
-    SelectedFont='Times New Roman';
-    SelectedFontSize=8;
-    SelectedLocation='Best';
+    SelectedFont = 'Times New Roman';
+    SelectedFontSize = 8;
+    LegendPosition = 'best';
 end
 
-cd(fold_user)
-Sheet_Ass=readcell(FileNameLandUsesAssociation,'Sheet','Association');
-CheckColor=cellfun(@ismissing,Sheet_Ass(2:end,3),'UniformOutput',false);
-AllLandUnique=Sheet_Ass(2:end,2);
+InfoDetectedExist = false;
+if exist('InfoDetectedSoilSlips.mat', 'file')
+    load('InfoDetectedSoilSlips.mat', 'InfoDetectedSoilSlips')
+    InfoDetectedExist = true;
+end
 
+%% For scatter dimension
+RefStudyArea = 0.035;
+% ExtentStudyArea = area(StudyAreaPolygon);
+ExtentStudyArea = prod(MaxExtremes-MinExtremes);
+RatioRef = ExtentStudyArea/RefStudyArea;
+PixelSize = .05/RatioRef;
+DetPixelSize = 2*PixelSize;
+
+%% Loading Excel
+cd(fold_user)
+Sheet_Ass = readcell(FileNameLandUsesAssociation,'Sheet','Association');
+CheckColor = cellfun(@ismissing,Sheet_Ass(2:end,3),'UniformOutput',false);
+AllLandUnique = Sheet_Ass(2:end,2);
 
 if all([CheckColor{:}])
-    answer = questdlg('No colors are set in the Excel association. Do you want to assign a random triplet', ...
-                	  'No color', ...
-                	  'Yes, thanks','No, for God!','No, for God!');
-    switch answer
+    % Fig = uifigure; % Remember to comment if in App version
+    Options = {'Yes, thanks', 'No, for God!'};
+    AssignRndColors = uiconfirm(Fig, ['No colors are set in the Excel association. ' ...
+                                      'Do you want to assign a random triplet'], ...
+                                     'Window type', 'Options',Options);
+    switch AssignRndColors
         case 'Yes, thanks'
-            LUColors=rand(length(CheckColor),3).*255;
+            LUColors = rand(length(CheckColor),3).*255;
         case 'No, for God!'
             return
     end
 else
-
-LUColors = cell2mat(cellfun(@(x) sscanf(x,'%d',[1 3]), ...
+    LUColors = cell2mat(cellfun(@(x) sscanf(x,'%d',[1 3]), ...
                         Sheet_Ass(2:end,3),'UniformOutput',false)); 
 end
 
-%%
+%% Plot based on user selection
 switch NumFigPlot
     case 1
-        filename1='AllLandUse';
-        f1=figure(1);
-        set(f1 , ...
-            'Color',[1 1 1],...
-            'PaperType','a4',...
-            'PaperSize',[29.68 20.98 ],...    
-            'PaperUnits', 'centimeters',...
-            'PaperPositionMode','manual',...
-            'PaperPosition', [0 1 7 5],...
-            'InvertHardcopy','off');
-        set( gcf ,'Name' , filename1);
+        filename1 = 'AllLandUse';
+        fig_lu_all = figure(1);
+        ax1 = axes(fig_lu_all);
+        hold(ax1,'on')
+
+        set(gcf, 'Name',filename1);
         
-        for i1=1:size(LandUsePolygonsStudyArea,2)
-            plot(LandUsePolygonsStudyArea(i1),'FaceColor',LUColors(i1,:)./255,'FaceAlpha',1,'EdgeColor','none')
-            hold on
+        PolygonsPlot = cell(1, size(LandUsePolygonsStudyArea,2));
+        for i1 = 1:size(LandUsePolygonsStudyArea,2)
+            PolygonsPlot{i1} = plot(LandUsePolygonsStudyArea(i1), 'FaceColor',LUColors(i1,:)./255, ...
+                                                                  'FaceAlpha',1, 'EdgeColor','none');
         end
+        
+        plot(StudyAreaPolygon, 'FaceColor','none', 'LineWidth',1.5)
 
-        
-        plot(StudyAreaPolygon,'FaceColor','none')
-        hold on
-        PlotScaleBar
-        
-        hleg1=legend(AllLandUnique,...
-                    'FontName',SelectedFont,...
-                    'FontSize',SelectedFontSize,...
-                    'Location',SelectedLocation,...
-                    'NumColumns',1);
-        
-        hleg1.ItemTokenSize(1)=4;
-        
-        xlim([MinExtremes(1),MinExtremes(1)+2*(MaxExtremes(1)-MinExtremes(1))])
-        ylim([MinExtremes(2)-0.0005,MaxExtremes(2)+0.0005]) 
-        
-        legend boxoff
-        legend('AutoUpdate','off')
+        fig_settings(fold0)
 
+        if InfoDetectedExist
+            hdetected = cellfun(@(x,y) scatter(x, y, DetPixelSize, '^k','Filled'), InfoDetectedSoilSlips(:,5), InfoDetectedSoilSlips(:,6));
+            uistack(hdetected,'top')
+        end
+     
+        if exist('LegendPosition', 'var')
+            LegendObjects = PolygonsPlot;
+            LegendCaption = cellstr(AllLandUnique);
+
+            if InfoDetectedExist
+                LegendObjects = [LegendObjects, {hdetected(1)}];
+                LegendCaption = [LegendCaption; {"Points Analyzed"}];
+            end
+
+            hleg1 = legend([LegendObjects{:}], ...
+                            LegendCaption, ...
+                           'FontName',SelectedFont, ...
+                           'FontSize',SelectedFontSize, ...
+                           'Location',LegendPosition, ...
+                           'NumColumns',1, ...
+                           'Box','off');
+            
+            hleg1.ItemTokenSize(1) = 3;
+            
+            legend('AutoUpdate','off')
+
+            fig_rescaler(fig_lu_all, hleg1, LegendPosition)
+        end
         
-        comprose(14.1068,37.65,8,0.015,0)
-        text(14.103,37.67,'N','FontName',SelectedFont,'FontSize',SelectedFontSize)
-        
-        
-        set(gca,'visible','off')
+        set(gca, 'visible','off')
 
         cd(fold_fig)
-        exportgraphics(f1,strcat(filename1,'.png'),'Resolution',600);
-
-%%
+        exportgraphics(fig_lu_all, strcat(filename1,'.png'), 'Resolution',600);
 
     case 2
-        filename2='ExcludedLandUse';
-        f2=figure(2);
-        set(f2 , ...
-            'Color',[1 1 1],...
-            'PaperType','a4',...
-            'PaperSize',[29.68 20.98 ],...    
-            'PaperUnits', 'centimeters',...
-            'PaperPositionMode','manual',...
-            'PaperPosition', [0 1 20 12],...
-            'InvertHardcopy','off');
-        set( gcf ,'Name' , filename2);
-        
-        axes2 = axes('Parent',f2); 
-        hold(axes2,'on');
+        filename2 = 'ExcludedLandUse';
+        fig_lu_excl = figure(2);
+        ax2 = axes(fig_lu_excl);
+        hold(ax2,'on')
+
+        set(gcf, 'Name',filename2);
 
         if OrthophotoAnswer
-            cellfun(@(x,y) geoshow(x,y,'FaceAlpha',.5),ZOrtho,ROrtho);
+            cellfun(@(x,y) geoshow(x,y, 'FaceAlpha',.5), ZOrtho, ROrtho);
         end
 
-        
-
-        for i1=1:size(IndexLandUsesToRemove,2)
-            hpol_LUexcluded(i1)=plot(LandUsePolygonsStudyArea(IndexLandUsesToRemove(i1)),...
-                'FaceColor',LUColors(IndexLandUsesToRemove(i1),:)./255,'FaceAlpha',1,'EdgeColor','none');
-            hold on
+        LUExcludedPlot = cell(1, size(IndexLandUsesToRemove,2));
+        for i1 = 1:size(IndexLandUsesToRemove,2)
+            LUExcludedPlot{i1} = plot(LandUsePolygonsStudyArea(IndexLandUsesToRemove(i1)), ...
+                                      'FaceColor',LUColors(IndexLandUsesToRemove(i1),:)./255, ...
+                                      'FaceAlpha',1, 'EdgeColor','none');
         end
         
-        plot(StudyAreaPolygon,'FaceColor','none')
-        hold on
-        PlotScaleBar
+        plot(StudyAreaPolygon, 'FaceColor','none', 'LineWidth',1.5)
         
+        fig_settings(fold0)
+
+        if InfoDetectedExist
+            hdetected = cellfun(@(x,y) scatter(x, y, DetPixelSize, '^k','Filled'), InfoDetectedSoilSlips(:,5), InfoDetectedSoilSlips(:,6));
+            uistack(hdetected,'top')
+        end
         
-        hleg1=legend(hpol_LUexcluded,...
-            AllLandUnique{IndexLandUsesToRemove},...
-            'FontName',SelectedFont,...
-            'FontSize',SelectedFontSize,...
-            'Location',SelectedLocation,...
-            'NumColumns',1);
-        
-        hleg1.ItemTokenSize(1)=4;
-        
-        
-        xlim([MinExtremes(1),MaxExtremes(1)])
-        ylim([MinExtremes(2)-0.0005,MaxExtremes(2)+0.0005])
-              
-        
-        legend('AutoUpdate','off')
-        
-        
-        
-        comprose(14.1068,37.65,8,0.015,0)
-        text(14.103,37.67,'N','FontName',SelectedFont,'FontSize',SelectedFontSize)
-        
-        
-        set(gca,'visible','off')
+        if exist('LegendPosition', 'var')
+            LegendObjects = LUExcludedPlot;
+            LegendCaption = cellstr(AllLandUnique{IndexLandUsesToRemove});
+
+            if InfoDetectedExist
+                LegendObjects = [LegendObjects, {hdetected(1)}];
+                LegendCaption = [LegendCaption; {"Points Analyzed"}];
+            end
+
+            hleg1 = legend([LegendObjects{:}], ...
+                            LegendCaption, ...
+                           'FontName',SelectedFont, ...
+                           'FontSize',SelectedFontSize, ...
+                           'Location',LegendPosition, ...
+                           'NumColumns',1);
+            
+            hleg1.ItemTokenSize(1) = 3;
+                         
+            legend('AutoUpdate','off')
+
+            fig_rescaler(fig_lu_excl, hleg1, LegendPosition)
+        end
+ 
+        set(gca, 'visible','off')
 
         cd(fold_fig)
-        exportgraphics(f2,strcat(filename2,'.png'),'Resolution',600);
+        exportgraphics(fig_lu_excl, strcat(filename2,'.png'), 'Resolution',600);
 end
 cd(fold0)
