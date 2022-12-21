cc9ec7bef4a1f16c2445d5140fc7833672a8e2d8
SalvatoreMisia <104531745+SalvatoreMisia@users.noreply.github.com>
2022-12-21 13:34:19 +0100

    Update PostUserA_StudyArea_Step3.m

--- APP/MODULES/PostUserA_StudyArea_Step3.m
+++ APP/MODULES/PostUserA_StudyArea_Step3.m
@@ -1,13 +1,24 @@
+% Fig = uifigure; % Remember to comment if in app version
+ProgressBar = uiprogressdlg(Fig, 'Title','Removal of excluded areas', ...
+                                 'Message','Reading file', 'Cancelable','on', ...
+                                 'Indeterminate','on');
+drawnow
+
+%% Removal of excluded areas
 cd(fold_var)
 load('LandUsesVariables');
 load('StudyAreaVariables');
 
 VariablesStudyArea = {'StudyAreaPolygonClean', 'StudyAreaPolygonExcluded'};
 
+ProgressBar.Indeterminate = 'off';
 if any(LandUseToRemoveSel ~= "None of these")
     IndexLandUsesToRemove = zeros(1,length(LandUseToRemoveSel));
-    for i6 = 1:length(LandUseToRemoveSel)
-        IndexLandUsesToRemove(i6) = find(strcmp(AllLandUnique,LandUseToRemoveSel{i6}));
+    for i1 = 1:length(LandUseToRemoveSel)
+        ProgressBar.Message = strcat("Removal of polygon n. ",num2str(i1)," of ", num2str(length(LandUseToRemoveSel)));
+        ProgressBar.Value = i1/length(LandUseToRemoveSel);
+
+        IndexLandUsesToRemove(i1) = find(strcmp(AllLandUnique,LandUseToRemoveSel{i1}));
     end
     LandToRemovePolygon = union([LandUsePolygonsStudyArea(IndexLandUsesToRemove)]);
     StudyAreaPolygonClean = subtract(StudyAreaPolygon, LandToRemovePolygon);
@@ -20,8 +31,13 @@ end
 VariablesLandUse = {'AllLandUnique', 'LandUsePolygonsStudyArea', 
                     'LandToRemovePolygon', 'IndexLandUsesToRemove'};
 
+ProgressBar.Indeterminate = 'on';
+ProgressBar.Message = 'Finising...';
+
 %% Saving..
 cd(fold_var)
 save('StudyAreaVariables.mat', VariablesStudyArea{:}, '-append');
-save('LandUsesVariables.mat', VariablesLandUse{:},'-append')
+save('LandUsesVariables.mat', VariablesLandUse{:}, '-append')
 cd(fold0)
+
+close(ProgressBar) % Fig instead of ProgressBar if in Standalone version
