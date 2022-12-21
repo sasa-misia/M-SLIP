866b6476b332763fb31ee0cfef79e70b85975f33
SalvatoreMisia <104531745+SalvatoreMisia@users.noreply.github.com>
2022-12-21 13:34:03 +0100

    Update PlotVegetationParameters.m

--- APP/MODULES/PlotVegetationParameters.m
+++ APP/MODULES/PlotVegetationParameters.m
@@ -15,6 +15,20 @@ else
     LegendPosition = 'Best';
 end
 
+InfoDetectedExist = false;
+if exist('InfoDetectedSoilSlips.mat', 'file')
+    load('InfoDetectedSoilSlips.mat', 'InfoDetectedSoilSlips')
+    InfoDetectedExist = true;
+end
+
+%% For scatter dimension
+RefStudyArea = 0.0417;
+ExtentStudyArea = area(StudyAreaPolygon);
+% ExtentStudyArea = prod(MaxExtremes-MinExtremes);
+RatioRef = ExtentStudyArea/RefStudyArea;
+PixelSize = .1/RatioRef;
+DetPixelSize = 2*PixelSize;
+
 %% Extraction of points inside study area
 xLongStudyArea          = cellfun(@(x,y) x(y), xLongAll, ...
                                            IndexDTMPointsInsideStudyArea, 'UniformOutput',false);
@@ -53,17 +67,17 @@ else
     % cr_uv = (DVCParameters{1});
     [CrUniqueUV, PosCrUniqueUV] = unique(DVCParameters{1});
 
-    [~, CorrectOrderCr] = sort(cr_uv); % Forse è qui il problema
+    % [~, CorrectOrderCr] = sort(cr_uv); % Forse è qui il problema
 
-    cr_uv = cr_uv(CorrectOrderCr);
+    % cr_uv = cr_uv(CorrectOrderCr);
 
     % BetaUnique = unique([DVCParameters{2}; BetaUnique]); Da modificare
     % per espandere gli estremi se raggiungi livelli più bassi o alti.
 
     [BetaUniqueUV, PosBetaUniqueUV] = unique(DVCParameters{2});
 
-    color_polygons = VU_DVCPlotColors{1}./255;
-    color_parameters = VU_DVCPlotColors{2}./255;
+    color_polygons = single(VU_DVCPlotColors{1})./255;
+    color_parameters = single(VU_DVCPlotColors{2})./255;
 
     color_parametersCr = color_parameters(PosCrUniqueUV,:);
     color_parametersBetaUniqueUV = color_parameters(PosBetaUniqueUV,:);
@@ -96,16 +110,6 @@ for i1 = 1:size(xLongStudyArea,2)
     end
 end
 
-%% Option for veg association
-% Fig = uifigure; % Remember to comment if in app version
-if AnswerAttributionVegetationParameter==0
-    ShowOver = false;
-else
-    ShowOver = uiconfirm(Fig, 'Do you want to show beta* of veg polygons on top and separately?', ...
-                               'Show veg', 'Options',{'Yes', 'No'});
-    if strcmp(ShowOver,'Yes'); ShowOver = true; else; ShowOver = false; end
-end
-
 %% Plot based on option choosed
 switch NumFigPlot
 
@@ -118,37 +122,47 @@ switch NumFigPlot
             ax_ind1 = axes(fig_veg);
             hold(ax_ind1,'on')
     
-%             set(fig_veg, ...
-%                 'Color',[1 1 1], ...
-%                 'PaperType','a4', ...
-%                 'PaperSize',[29.68 20.98 ], ...    
-%                 'PaperUnits','centimeters', ...
-%                 'PaperPositionMode','manual', ...
-%                 'PaperPosition',[0 1 13 11], ...
-%                 'InvertHardcopy','off');
-    
             set(gcf, 'Name',filename1);  
             
+            hplot_veg = cell(1, size(SelectedVeg,1));
             for i2 = 1:size(SelectedVeg,1)
-                plot(VegPolygonsStudyArea(SelectedVeg(i2)), 'FaceColor',color_polygons(i2,:), 'Parent',ax_ind1, ...
-                                                            'FaceAlpha',0.9, 'EdgeColor','none')
+                hplot_veg{i2} = plot(VegPolygonsStudyArea(SelectedVeg(i2)), ...
+                                                'FaceColor',color_polygons(i2,:), 'Parent',ax_ind1, ...
+                                                'FaceAlpha',0.9, 'EdgeColor','none');
             end
             
-            if exist('LegendPosition', 'var')
-                leg1 = legend(VUAbbr, ...
-                              'AutoUpdate','off', ...
-                              'NumColumns',1, ...
-                              'FontName',SelectedFont, ...
-                              'FontSize',SelectedFontSize, ...
-                              'Location',LegendPosition, ...
-                              'Box','off');
-        
-                leg1.ItemTokenSize(1) = 10;
-            end
-    
             plot(MunPolygon, 'FaceColor','none', 'LineWidth',1.5, 'Parent',ax_ind1)
     
             fig_settings(fold0)
+
+            if InfoDetectedExist
+                hdetected = cellfun(@(x,y) scatter(x, y, DetPixelSize, '^k','Filled'), ...
+                                        InfoDetectedSoilSlips(:,5), InfoDetectedSoilSlips(:,6));
+                uistack(hdetected,'top')
+            end
+
+            if exist('LegendPosition', 'var')
+                LegendObjects = hplot_veg;
+                LegendCaption = cellstr(VUAbbr);
+
+                if InfoDetectedExist
+                    LegendObjects = [LegendObjects, {hdetected(1)}];
+                    LegendCaption = [LegendCaption; {"Points Analyzed"}];
+                end
+
+                hleg1 = legend([LegendObjects{:}], ...
+                               LegendCaption, ...
+                               'AutoUpdate','off', ...
+                               'NumColumns',1, ...
+                               'FontName',SelectedFont, ...
+                               'FontSize',SelectedFontSize, ...
+                               'Location',LegendPosition, ...
+                               'Box','off');
+        
+                hleg1.ItemTokenSize(1) = 5;
+
+                fig_rescaler(fig_veg, hleg1, LegendPosition)
+            end
             
             set(gca, 'visible','off')
             
@@ -162,47 +176,55 @@ switch NumFigPlot
         ax_ind2 = axes(fig_root);
         hold(ax_ind2,'on')
 
-%         set(fig_root, ...
-%             'Color',[1 1 1], ...
-%             'PaperType','a4', ...
-%             'PaperSize',[29.68 20.98 ], ...    
-%             'PaperUnits','centimeters', ...
-%             'PaperPositionMode','manual', ...
-%             'PaperPosition',[0 1 13 11], ...
-%             'InvertHardcopy','off');
-
         set(gcf, 'Name',filename2);  
 
+        hcr = cell(1, size(xLongStudyArea,2));
         for i1 = 1:size(xLongStudyArea,2)
             for i2 = 1:UVNumber
-                hcr(i2) = scatter(xLongStudyArea{i1}(cr_case{i2,i1}), ...
+                hcr{i2} = scatter(xLongStudyArea{i1}(cr_case{i2,i1}), ...
                                   yLatStudyArea{i1}(cr_case{i2,i1}), ...
-                                  .1/RatioRef, 'o', 'Parent',ax_ind2, ...
-                                  'MarkerFaceColor',color_parametersCr(i2,:), ...
-                                  'MarkerEdgeColor','none');
+                                  PixelSize, 'o', 'Parent',ax_ind2, ...
+                                             'MarkerFaceColor',color_parametersCr(i2,:), ...
+                                             'MarkerEdgeColor','none');
              end
-        end
-        
-        if exist('LegendPosition', 'var')
-            leg_cr = cellstr(num2str(CrUniqueUV)); 
-            leg1 = legend(hcr, ...
-                          leg_cr{:}, ...
-                          'NumColumns',1, ...
-                          'FontName',SelectedFont, ...
-                          'FontSize',SelectedFontSize, ...
-                          'Location',LegendPosition);
-    
-            legend('AutoUpdate','off');
-            legend boxoff
-            
-            leg1.ItemTokenSize(1) = 10;
-            title(leg1, '{\it c_r} [kPa]', 'FontName',SelectedFont, ...
-                        'FontSize',SelectedFontSize*1.2, 'FontWeight','bold')
         end
    
         plot(MunPolygon, 'FaceColor','none', 'LineWidth',1, 'Parent',ax_ind2);
         
         fig_settings(fold0)
+
+        if InfoDetectedExist
+            hdetected = cellfun(@(x,y) scatter(x, y, DetPixelSize, '^k','Filled'), ...
+                                    InfoDetectedSoilSlips(:,5), InfoDetectedSoilSlips(:,6));
+            uistack(hdetected,'top')
+        end
+
+        if exist('LegendPosition', 'var')
+            LegendObjects = hcr;
+            LegendCaption = cellstr(num2str(CrUniqueUV)); 
+
+            if InfoDetectedExist
+                LegendObjects = [LegendObjects, {hdetected(1)}];
+                LegendCaption = [LegendCaption; {"Points Analyzed"}];
+            end
+
+            hleg1 = legend([LegendObjects{:}], ...
+                           LegendCaption, ...
+                           'NumColumns',1, ...
+                           'FontName',SelectedFont, ...
+                           'FontSize',SelectedFontSize, ...
+                           'Location',LegendPosition, ...
+                           'Box','off');
+    
+            legend('AutoUpdate','off');
+            
+            hleg1.ItemTokenSize(1) = 10;
+
+            title(hleg1, '{\it c_r} [kPa]', 'FontName',SelectedFont, ...
+                         'FontSize',SelectedFontSize*1.2, 'FontWeight','bold')
+
+            fig_rescaler(fig_root, hleg1, LegendPosition)
+        end
         
         set(gca, 'visible','off')
 
@@ -210,60 +232,81 @@ switch NumFigPlot
         exportgraphics(fig_root, strcat(filename2,'.png'), 'Resolution',600);
 
     case 3
+        % Option for veg association
+        % Fig = uifigure; % Remember to comment if in app version
+        if AnswerAttributionVegetationParameter==0
+            ShowOver = false;
+        else
+            ShowOver = uiconfirm(Fig, 'Do you want to show beta* of veg polygons on top and separately?', ...
+                                      'Show veg', 'Options',{'Yes', 'No'});
+            if strcmp(ShowOver,'Yes'); ShowOver = true; else; ShowOver = false; end
+        end
+        
         filename3 = 'BetastarMap';
         fig_beta = figure(3);
         ax_ind3 = axes(fig_beta);
         ax_ind3_UV = axes(fig_beta, 'Visible','off');
         hold(ax_ind3,'on')
 
-%         set(fig_beta, ...
-%             'Color',[1 1 1], ...
-%             'PaperType','a4', ...
-%             'PaperSize',[29.68 20.98 ], ...    
-%             'PaperUnits','centimeters', ...
-%             'PaperPositionMode','manual', ...
-%             'PaperPosition',[0 1 13 11], ...
-%             'InvertHardcopy','off');
-
         set(fig_beta, 'Name',filename3);  
         
+        hbetastar = cell(1, size(xLongStudyArea,2));
         for i1 = 1:size(xLongStudyArea,2)
             for i2 = length(BetaUnique):-1:1
                 if ~isempty(betastar_case{i2,i1})
-                    hbetastar(i2) = scatter(xLongStudyArea{i1}(betastar_case{i2,i1}), ...
+                    hbetastar{i2} = scatter(xLongStudyArea{i1}(betastar_case{i2,i1}), ...
                                             yLatStudyArea{i1}(betastar_case{i2,i1}), ...
-                                            .1/RatioRef, 'o', 'Parent',ax_ind3, ...
+                                            PixelSize, 'o', 'Parent',ax_ind3, ...
                                             'MarkerFaceColor',color_parametersBetaUnique(i2,:), ...
                                             'MarkerEdgeColor','none');
                 end
             end
 
             if ShowOver
+                hbetastarUV = cell(1, length(BetaUniqueUV));
                 for i2 = length(BetaUniqueUV):-1:1
                     if ~isempty(betastar_caseUV{i2,i1})
-                        hbetastarUV(i2) = scatter(xLongStudyArea{i1}(betastar_caseUV{i2,i1}), ...
-                                                yLatStudyArea{i1}(betastar_caseUV{i2,i1}), ...
-                                                .08/RatioRef, '^', 'Parent',ax_ind3, ...
-                                                'MarkerFaceColor',color_parametersBetaUniqueUV(i2,:), ...
-                                                'MarkerEdgeColor','none');
+                        hbetastarUV{i2} = scatter(xLongStudyArea{i1}(betastar_caseUV{i2,i1}), ...
+                                                  yLatStudyArea{i1}(betastar_caseUV{i2,i1}), ...
+                                                  PixelSize, '^', 'Parent',ax_ind3, ...
+                                                  'MarkerFaceColor',color_parametersBetaUniqueUV(i2,:), ...
+                                                  'MarkerEdgeColor','none');
                     end
                 end
             end
         end
         
+        plot(MunPolygon, 'FaceColor','none', 'LineWidth',1, 'Parent',ax_ind3);
+
+        set(fig_beta, 'CurrentAxes',ax_ind3)
+        fig_settings(fold0)
+
+        if InfoDetectedExist
+            hdetected = cellfun(@(x,y) scatter(x, y, DetPixelSize, '^k','Filled'), ...
+                                    InfoDetectedSoilSlips(:,5), InfoDetectedSoilSlips(:,6));
+            uistack(hdetected,'top')
+        end
+
         % Legend 1
         if exist('LegendPosition', 'var')
+            LegendObjects1 = hbetastar;
+            ValuesBeta = round(BetaUnique,2);
+            LegendCaption1 = cellstr([strcat(string(ValuesBeta(1:end-1)), " - ", string(ValuesBeta(2:end))); "1"]);
+
+            if InfoDetectedExist
+                LegendObjects1 = [LegendObjects1, {hdetected(1)}];
+                LegendCaption1 = [LegendCaption1; {"Points Analyzed"}];
+            end
+
 %             LegendObjects = [hbetastar, hbetastarUV];
 %             ValuesBeta = round(BetaUnique,2);
 %             ValuesBetaUV = round(BetaUniqueUV,2);
 %             LegendCaption = [strcat(string(ValuesBeta(1:end-1)), " - ", string(ValuesBeta(2:end)))
 %                              "1"
 %                              string(ValuesBetaUV)];
-            LegendObjects1 = hbetastar;
-            ValuesBeta = round(BetaUnique,2);
-            LegendCaption1 = [strcat(string(ValuesBeta(1:end-1)), " - ", string(ValuesBeta(2:end))); "1"];
 
-            leg1 = legend(LegendObjects1, ...
+            leg1 = legend(ax_ind3, ...
+                          [LegendObjects1{:}], ...
                           LegendCaption1, ...
                           'NumColumns',1, ...
                           'FontName',SelectedFont, ...
@@ -276,12 +319,9 @@ switch NumFigPlot
             
             title(leg1, '1-{\it \beta^*} [-]', 'FontName',SelectedFont, ...
                         'FontSize',SelectedFontSize*1.2, 'FontWeight','bold')
-        end
-        
-        plot(MunPolygon, 'FaceColor','none', 'LineWidth',1, 'Parent',ax_ind3);
 
-        set(fig_beta, 'CurrentAxes',ax_ind3)
-        fig_settings(fold0)
+            fig_rescaler(fig_beta, leg1, LegendPosition)
+        end
 
         % Legend 2, is important to do this operation after fig_settings!
         if exist('LegendPosition', 'var') && ShowOver
@@ -296,7 +336,7 @@ switch NumFigPlot
             leg2Pos(4) = leg2Pos(4)*RatioLeg;
 
             leg2 = legend(ax_ind3_UV, ...
-                          LegendObjects2, ...
+                          [LegendObjects2{:}], ...
                           LegendCaption2, ...
                           'NumColumns',1, ...
                           'FontName',SelectedFont, ...
