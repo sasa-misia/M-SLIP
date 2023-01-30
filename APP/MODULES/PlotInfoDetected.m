596061d341cdf03d69881ebb7a21734fba5cd0ab
SalvatoreMisia <104531745+SalvatoreMisia@users.noreply.github.com>
2023-01-30 17:14:05 +0100

    Update PlotInfoDetected.m

--- APP/MODULES/PlotInfoDetected.m
+++ APP/MODULES/PlotInfoDetected.m
@@ -1,3 +1,9 @@
+% Fig = uifigure; % Remember to comment if in app version
+ProgressBar = uiprogressdlg(Fig, 'Title','Reading data', ...
+                                 'Message','Reading files...', 'Cancelable','off', ...
+                                 'Indeterminate','on');
+drawnow
+
 %% Loading Files
 cd(fold_var)
 load('StudyAreaVariables.mat')
@@ -23,46 +29,133 @@ cd(fold_raw_ctr)
 Files = sort(string([{dir('*.tif').name}, ...
                      {dir('*.tfw').name}, ...
                      {dir('*.asc').name}, ...
-                     {dir('*.img').name}]));
+                     {dir('*.img').name}, ...
+                     {dir('*.txt').name}, ...
+                     {dir('*.png').name}, ...
+                     {dir('*.pgw').name}, ...
+                     {dir('*.jpg').name}, ...
+                     {dir('*.jgw').name}]));
 Choice = listdlg('PromptString',{'Choose a file:',''}, ...
                   'ListString',Files);
 FileNameCTR = string(Files(Choice));
 
+drawnow % Remember to remove if in Standalone version
+figure(Fig) % Remember to remove if in Standalone version
+
 if any(contains(FileNameCTR,'tif')) && any(contains(FileNameCTR,'tfw'))
-    DTMType = 0;
-else
-    DTMType = 1;
+    CTRType = 0;
+elseif all(contains(FileNameCTR,'tif'))
+    CTRType = 1;
+elseif all(contains(FileNameCTR,'img'))
+    CTRType = 2;
+elseif any(contains(FileNameCTR,'jpg'))
+    CTRType = 3;
+    YetReferenced = false;
+    if any(contains(FileNameCTR,'jgw'))
+        YetReferenced = true;
+    end
 end
 
-switch DTMType
+switch CTRType
     case 0
         NameFile1 = FileNameCTR(contains(FileNameCTR,'tif'));
         NameFile2 = FileNameCTR(contains(FileNameCTR,'tfw'));
+
     case 1
         NameFile1 = FileNameCTR;
+
+    case 2
+        NameFile1 = FileNameCTR;
+
+    case 3
+        NameFile1 = FileNameCTR(contains(FileNameCTR,'jpg'));
+        if YetReferenced
+            NameFile2 = FileNameCTR(contains(FileNameCTR,'jgw'));
+        else
+            NameFile3 = FileNameCTR(contains(FileNameCTR,'txt'));
+        end
+
+    otherwise
+        error('Filetype not supported!')
 end
 
 %% Raster CTR Processing
+ProgressBar.Indeterminate = 'off';
+
 [xLongCTRStudy, yLatCTRStudy, GrayScaleCTRStudy] = deal(cell(1,length(NameFile1)));
 
 [pp1, ee1] = getnan2([StudyAreaPolygon.Vertices; nan, nan]);
 for i1 = 1:length(NameFile1)
-    switch DTMType
+
+    ProgressBar.Message = strcat("Creation of CTR n. ",num2str(i1)," of ", num2str(length(NameFile1)));
+    ProgressBar.Value = i1/length(NameFile1);
+
+    switch CTRType
         case 0
             A = imread(NameFile1(i1));
-            R = worldfileread(NameFile2(i1), 'planar', size(A));  
+            R = worldfileread(NameFile2(i1), 'planar', size(A));
+
         case 1
             [A,R] = readgeoraster(NameFile1(i1), 'OutputType','native');
+
+        case 2
+            [A,R] = readgeoraster(NameFile1(i1), 'OutputType','native');
+
+        case 3
+            A = imread(NameFile1(i1));
+
+            if size(A, 3) == 3 && isequal(A(:, :, 1), A(:, :, 2), A(:, :, 3))
+                A = A(:, :, 1);
+            elseif size(A, 3) == 3 && ~isequal(A(:, :, 1), A(:, :, 2), A(:, :, 3))
+                A = rgb2gray(A);
+            end
+
+            if ~YetReferenced % REMEMBER TO MODIFY FOR DIFFERENT CRS!
+                fileID = fopen(NameFile3(i1), 'r');
+                ImageExt = fscanf(fileID, '%f');
+                fclose(fileID);
+
+                LatN  = ImageExt(3);
+                LongW = ImageExt(2);
+                LatS  = ImageExt(1);
+                LongE = ImageExt(4);
+
+                EPSGDest = projcrs(32633); % CHANGE THIS LINE
+
+                [xW, yN] = projfwd(EPSGDest, LatN, LongW);
+                [xE, yS] = projfwd(EPSGDest, LatS, LongE);
+
+                dLat  = -(LatN-LatS)/size(A, 1); % KEEP ATTENTION
+                dLong = (LongE-LongW)/size(A, 2); % KEEP ATTENTION
+                dy = -(yN-yS)/size(A, 1);
+                dx = (xE-xW)/size(A, 2);
+
+                WorldFileContGeo  = [dLong; 0; 0; dLat; LongW; LatN];
+                WorldFileContPlan = [dx; 0; 0; dy; xW; yN];
+                WorldFileame  = getworldfilename(NameFile1(i1));
+
+                fileID = fopen(WorldFileame, 'w');
+                ImageExt = fprintf(fileID, '%f\n', WorldFileContPlan);
+                fclose(fileID);
+
+                % [~, NameFile2NoExt, ~] = fileparts(NameFile3(i1));
+                R = worldfileread(WorldFileame, 'planar', size(A));
+            else
+                R = worldfileread(NameFile2(i1), 'planar', size(A));
+            end
+
     end
 
-    if isempty(R.ProjectedCRS) && i1==1
+    if string(R.CoordinateSystemType)=="planar" && isempty(R.ProjectedCRS) && i1==1
         EPSG = str2double(inputdlg({["Set DTM EPSG"
                                      "For Example:"
                                      "Sicily -> 32633"
                                      "Emilia Romagna -> 25832"]}, '', 1, {'32633'}));
         R.ProjectedCRS = projcrs(EPSG);
-    elseif isempty(R.ProjectedCRS) && i1>1
+    elseif string(R.CoordinateSystemType)=="planar" && isempty(R.ProjectedCRS) && i1>1
         R.ProjectedCRS = projcrs(EPSG);
+    elseif string(R.CoordinateSystemType)=="geographic"
+        R.GeographicCRS = geocrs(4326);
     end
 
     [x_lim,y_lim] = mapoutline(R, size(A));
@@ -122,14 +215,19 @@ yLatCTRStudy(EmptyIndexCTRPointsInsideStudyArea)                    = [];
 GrayScaleCTRStudy(EmptyIndexCTRPointsInsideStudyArea)               = [];
 
 %% Saving...
+ProgressBar.Indeterminate = 'on';
+ProgressBar.Message = 'Saving created files...';
+
 cd(fold_var)
 VariablesCTR = {'xLongCTRStudy', 'yLatCTRStudy', 'GrayScaleCTRStudy', 'NameFileIntersecated'};
 save('StudyCTR.mat', VariablesCTR{:});
 cd(fold0)
 
 %% Plot for check
-SkipCheck = false;
-if SkipCheck
+SkipCheck = true;
+if ~SkipCheck
+    ProgressBar.Message = 'Creating plot for check...';
+
     fig_check = figure(2);
     ax_check = axes(fig_check);
     hold(ax_check,'on')
@@ -148,10 +246,11 @@ if SkipCheck
 end
 
 %% Request for Elevation and Slope
-% Fig = uifigure; % Remember to comment if in app version
+ProgressBar.Message = 'Figure options...';
+
 Options = {'All', 'Selected class', 'Elevation + Slope', 'Aspect'};
 SelPlots = uiconfirm(Fig, 'What do you want to plot?', ...
-                        'Tables plot', 'Options',Options);
+                          'Tables plot', 'Options',Options);
 switch SelPlots
     case 'All'
         SelPlots = 1;
@@ -169,7 +268,30 @@ ShowPlots = uiconfirm(Fig, 'Do you want to show plots?', ...
 if strcmp(ShowPlots,'Yes'); ShowPlots = true; else; ShowPlots = false; end
 
 %% Figure Creation
-rng(15) % For havenig the same color palet everytime
+Options = {'Color Comb 1', 'Color Comb 2', 'Color Comb 3', 'Color Comb 4'};
+ColorComb = uiconfirm(Fig, 'What color combination do you want to use?', ...
+                           'Color combination', 'Options',Options);
+switch ColorComb
+    case 'Color Comb 1'
+        rng(15)  % For havenig the same color palet everytime
+    case 'Selected class'
+        rng(30)  % For havenig the same color palet everytime
+    case 'Elevation + Slope'
+        rng(115) % For havenig the same color palet everytime
+    case 'Aspect'
+        rng(175) % For havenig the same color palet everytime
+end
+
+TransparencyValues = inputdlg({'Indicate transparency of background (from 0 to 1):'
+                               'Indicate transparency of top layer (from 0 to 1):'},'', ...
+                               1, {'0.8', '0.5'});
+
+BTrans = eval(TransparencyValues{1});
+TTrans = eval(TransparencyValues{2});
+
+drawnow % Remember to remove if in Standalone version
+figure(Fig) % Remember to remove if in Standalone version
+
 RefStudyArea = 0.035;
 
 xLongCTRTot      = cat(1,xLongCTRStudy{:});
@@ -188,8 +310,13 @@ Classes(IndClassesNS) = [];
 PlotColors = arrayfun(@(x) rand(1, 3), Classes, 'UniformOutput',false);
 
 warning('off')
+ProgressBar.Indeterminate = 'off';
 
 for i1 = 1:length(PolWindow)
+
+    ProgressBar.Message = strcat("Creation of figure n. ",num2str(i1)," of ", num2str(length(PolWindow)));
+    ProgressBar.Value = i1/length(PolWindow);
+
     %% Preliminary operations
     ExtentStudyArea = area(PolWindow(i1));
     ExtremesPlot = PolWindow(i1).Vertices;
@@ -200,12 +327,43 @@ for i1 = 1:length(PolWindow)
     % CTR Preliminary operation
     IndexCTRInPolWin = find(inpoly([xLongCTRTot, yLatCTRTot], ppWin, eeWin)==1);
     if ~isempty(IndexCTRInPolWin)
-        xLongCTRPar     = xLongCTRTot(IndexCTRInPolWin);
-        yLatCTRPar      = yLatCTRTot(IndexCTRInPolWin);
-        GrayScaleCTRPar = GrayScaleCTRTot(IndexCTRInPolWin);
+
+        OverlapDTMs = false; % GIVE THE CHOICE TO USER
+        if OverlapDTMs
+            xLongCTRPar     = xLongCTRTot(IndexCTRInPolWin);
+            yLatCTRPar      = yLatCTRTot(IndexCTRInPolWin);
+            GrayScaleCTRPar = GrayScaleCTRTot(IndexCTRInPolWin);
+        else
+            IndexCTRInPolWinSingle = cellfun(@(x,y) find(inpoly([x, y], ppWin, eeWin)==1), xLongCTRStudy, yLatCTRStudy, 'UniformOutput',false);
+            [~, CTRWithMaxPoint] = max(cellfun(@length, IndexCTRInPolWinSingle));
+
+            xLongCTRPar     = xLongCTRStudy{CTRWithMaxPoint}(IndexCTRInPolWinSingle{CTRWithMaxPoint});
+            yLatCTRPar      = yLatCTRStudy{CTRWithMaxPoint}(IndexCTRInPolWinSingle{CTRWithMaxPoint});
+            GrayScaleCTRPar = GrayScaleCTRStudy{CTRWithMaxPoint}(IndexCTRInPolWinSingle{CTRWithMaxPoint});
+        end
     
-        xLongCTRToPlot  = xLongCTRPar(~logical(GrayScaleCTRPar));
-        yLatCTRToPlot   = yLatCTRPar(~logical(GrayScaleCTRPar));
+        ColorUniqueValues = unique(GrayScaleCTRPar);
+        if length(ColorUniqueValues) == 2
+            ColorType = "BW";
+        elseif length(ColorUniqueValues)>2 && max(ColorUniqueValues)>1
+            ColorType = "GrayNotScaled";
+        elseif length(ColorUniqueValues)>2 && max(ColorUniqueValues)<=1
+            ColorType = "GrayScaled";
+        else
+            error('Error with gray color of CTR')
+        end
+
+        switch ColorType
+            case "BW"
+                xLongCTRToPlot = xLongCTRPar(~logical(GrayScaleCTRPar));
+                yLatCTRToPlot  = yLatCTRPar(~logical(GrayScaleCTRPar));
+            case "GrayNotScaled"
+                GrayScaleUniqueCTR = num2cell(unique(GrayScaleCTRPar));
+                IndexToPlot = cellfun(@(x) find(x == GrayScaleCTRPar), GrayScaleUniqueCTR, 'UniformOutput',false);
+            case "GrayScaled"
+                error('Not yet implemented')
+        end
+
     else
         strcat("Municipality: ", string(InfoDetectedSoilSlips{i1,1}), " does not have CTR")
     end
@@ -227,14 +385,22 @@ for i1 = 1:length(PolWindow)
         Filename1 = strcat("Classes for point n. ",string(i1));
         title(strcat( "Comune: ", string(InfoDetectedSoilSlips{i1,1})), ...
               'FontName',SelectedFont, 'FontSize',SelectedFontSize*1.4, 'Parent',ax_ind1)
-        subtitle(strcat( "Cod. IOP: ", string(InfoDetectedSoilSlips{i1,2})), ...
+        subtitle(strcat( "Cod. IOP: ", string(strrep(InfoDetectedSoilSlips{i1,2}, '_', ' ')) ), ...
                  'FontName',SelectedFont, 'FontSize',SelectedFontSize*1.2, 'Parent',ax_ind1)
         set(fig_class, 'Name',Filename1);
     
         % Plot CTR
         if ~isempty(IndexCTRInPolWin)
-            PlotCTR1 = scatter(xLongCTRToPlot, yLatCTRToPlot, 0.5, 'Marker','s', 'MarkerFaceColor','k', ...
-                               'MarkerEdgeColor','none', 'MarkerFaceAlpha',1, 'Parent',ax_ind1);
+            switch ColorType
+                case "BW"
+                    PlotCTR1 = scatter(xLongCTRToPlot, yLatCTRToPlot, 0.5, 'Marker','s', 'MarkerFaceColor','k', ...
+                                       'MarkerEdgeColor','none', 'MarkerFaceAlpha',BTrans, 'Parent',ax_ind1);
+                case {"GrayNotScaled", "GrayScaled"}
+                    PlotCTR1 = cellfun(@(x,z) scatter(xLongCTRPar(x), yLatCTRPar(x), 0.9, ...
+                                                      'Marker','s', 'MarkerFaceColor',single([z, z, z])./255, ...
+                                                      'MarkerEdgeColor','none', 'MarkerFaceAlpha',BTrans, 'Parent',ax_ind1), ...
+                                              IndexToPlot, GrayScaleUniqueCTR, 'UniformOutput',false);
+            end
         end
     
         % Plot Classes (LU)
@@ -251,7 +417,7 @@ for i1 = 1:length(PolWindow)
     
         PlotClass = cellfun(@(x,z) scatter(PointsNearLong(x), PointsNearLat(x), 0.1*PixelSize, ...
                                            'Marker','o', 'MarkerFaceColor',z, 'MarkerEdgeColor','none', ...
-                                           'MarkerFaceAlpha',0.35, 'Parent',ax_ind1), ...
+                                           'MarkerFaceAlpha',TTrans, 'Parent',ax_ind1), ...
                                    IndClasses, PlotColorsLoc, 'UniformOutput',false);
     
         % Plot Contour, detected point & extra settings
@@ -306,19 +472,27 @@ for i1 = 1:length(PolWindow)
         Filename2 = strcat("Morphology of point n. ",string(i1));
         title(strcat( "Comune: ", string(InfoDetectedSoilSlips{i1,1})), ...
               'FontName',SelectedFont, 'FontSize',SelectedFontSize*1.4, 'Parent',ax_ind2)
-        subtitle(strcat( "Cod. IOP: ", string(InfoDetectedSoilSlips{i1,2})), ...
+        subtitle(strcat( "Cod. IOP: ", string(strrep(InfoDetectedSoilSlips{i1,2}, '_', ' ')) ), ...
                  'FontName',SelectedFont, 'FontSize',SelectedFontSize*1.2, 'Parent',ax_ind2)
         set(fig_morph, 'Name',Filename2);
     
         if ~isempty(IndexCTRInPolWin)
-            PlotCTR2 = scatter(xLongCTRToPlot, yLatCTRToPlot, 0.5, 'Marker','s', 'MarkerFaceColor','k', ...
-                                   'MarkerEdgeColor','none', 'MarkerFaceAlpha',1, 'Parent',ax_ind2);
+            switch ColorType
+                case "BW"
+                    PlotCTR2 = scatter(xLongCTRToPlot, yLatCTRToPlot, 0.5, 'Marker','s', 'MarkerFaceColor','k', ...
+                                           'MarkerEdgeColor','none', 'MarkerFaceAlpha',BTrans, 'Parent',ax_ind2);
+                case {"GrayNotScaled", "GrayScaled"}
+                    PlotCTR2 = cellfun(@(x,z) scatter(xLongCTRPar(x), yLatCTRPar(x), 0.9, ...
+                                                      'Marker','s', 'MarkerFaceColor',single([z, z, z])./255, ...
+                                                      'MarkerEdgeColor','none', 'MarkerFaceAlpha',BTrans, 'Parent',ax_ind2), ...
+                                              IndexToPlot, GrayScaleUniqueCTR, 'UniformOutput',false);
+            end
         end
     
         PointsNearAlt  = cell2mat(InfoPointsNearDetectedSoilSlips{i1,4}(:,5));
         PointsNearAlt  = PointsNearAlt(IndexPointsInPolWin);
     
-        PlotMorph = fastscatter2(PointsNearLong, PointsNearLat, PointsNearAlt, 'FaceAlpha',0.4, 'Parent',ax_ind2);
+        PlotMorph = fastscatter2(PointsNearLong, PointsNearLat, PointsNearAlt, 'FaceAlpha',TTrans, 'Parent',ax_ind2);
     
         colormap(ax_ind2,'pink')
         LimitsCol = linspace(min(PointsNearAlt), max(PointsNearAlt), 5);
@@ -363,19 +537,27 @@ for i1 = 1:length(PolWindow)
         Filename3 = strcat("Slope of point n. ",string(i1));
         title(strcat( "Comune: ", string(InfoDetectedSoilSlips{i1,1})), ...
               'FontName',SelectedFont, 'FontSize',SelectedFontSize*1.4, 'Parent',ax_ind3)
-        subtitle(strcat( "Cod. IOP: ", string(InfoDetectedSoilSlips{i1,2})), ...
+        subtitle(strcat( "Cod. IOP: ", string(strrep(InfoDetectedSoilSlips{i1,2}, '_', ' ')) ), ...
                  'FontName',SelectedFont, 'FontSize',SelectedFontSize*1.2, 'Parent',ax_ind3)
         set(fig_slope, 'Name',Filename3);
     
         if ~isempty(IndexCTRInPolWin)
-            PlotCTR2 = scatter(xLongCTRToPlot, yLatCTRToPlot, 0.5, 'Marker','s', 'MarkerFaceColor','k', ...
-                                   'MarkerEdgeColor','none', 'MarkerFaceAlpha',1, 'Parent',ax_ind3);
+            switch ColorType
+                case "BW"
+                    PlotCTR3 = scatter(xLongCTRToPlot, yLatCTRToPlot, 0.5, 'Marker','s', 'MarkerFaceColor','k', ...
+                                           'MarkerEdgeColor','none', 'MarkerFaceAlpha',BTrans, 'Parent',ax_ind3);
+                case {"GrayNotScaled", "GrayScaled"}
+                    PlotCTR3 = cellfun(@(x,z) scatter(xLongCTRPar(x), yLatCTRPar(x), 0.9, ...
+                                                      'Marker','s', 'MarkerFaceColor',single([z, z, z])./255, ...
+                                                      'MarkerEdgeColor','none', 'MarkerFaceAlpha',BTrans, 'Parent',ax_ind3), ...
+                                              IndexToPlot, GrayScaleUniqueCTR, 'UniformOutput',false);
+            end
         end
     
         PointsNearSlope  = cell2mat(InfoPointsNearDetectedSoilSlips{i1,4}(:,6));
         PointsNearSlope  = PointsNearSlope(IndexPointsInPolWin);
     
-        PlotSlope = fastscatter2(PointsNearLong, PointsNearLat, PointsNearSlope, 'FaceAlpha',0.4, 'Parent',ax_ind3);
+        PlotSlope = fastscatter2(PointsNearLong, PointsNearLat, PointsNearSlope, 'FaceAlpha',TTrans, 'Parent',ax_ind3);
     
         colormap(ax_ind3,'cool')
         LimitsCol = linspace(min(PointsNearSlope), max(PointsNearSlope), 5);
@@ -421,19 +603,27 @@ for i1 = 1:length(PolWindow)
         Filename4 = strcat("Aspect of point n. ",string(i1));
         title(strcat( "Comune: ", string(InfoDetectedSoilSlips{i1,1})), ...
               'FontName',SelectedFont, 'FontSize',SelectedFontSize*1.4, 'Parent',ax_ind4)
-        subtitle(strcat( "Cod. IOP: ", string(InfoDetectedSoilSlips{i1,2})), ...
+        subtitle(strcat( "Cod. IOP: ", string(strrep(InfoDetectedSoilSlips{i1,2}, '_', ' ')) ), ...
                  'FontName',SelectedFont, 'FontSize',SelectedFontSize*1.2, 'Parent',ax_ind4)
         set(fig_aspect, 'Name',Filename4);
     
         if ~isempty(IndexCTRInPolWin)
-            PlotCTR2 = scatter(xLongCTRToPlot, yLatCTRToPlot, 0.5, 'Marker','s', 'MarkerFaceColor','k', ...
-                                   'MarkerEdgeColor','none', 'MarkerFaceAlpha',1, 'Parent',ax_ind4);
+            switch ColorType
+                case "BW"
+                    PlotCTR4 = scatter(xLongCTRToPlot, yLatCTRToPlot, 0.5, 'Marker','s', 'MarkerFaceColor','k', ...
+                                           'MarkerEdgeColor','none', 'MarkerFaceAlpha',BTrans, 'Parent',ax_ind4);
+                case {"GrayNotScaled", "GrayScaled"}
+                    PlotCTR4 = cellfun(@(x,z) scatter(xLongCTRPar(x), yLatCTRPar(x), 0.9, ...
+                                                      'Marker','s', 'MarkerFaceColor',single([z, z, z])./255, ...
+                                                      'MarkerEdgeColor','none', 'MarkerFaceAlpha',BTrans, 'Parent',ax_ind4), ...
+                                              IndexToPlot, GrayScaleUniqueCTR, 'UniformOutput',false);
+            end
         end
     
         PointsNearAspect  = cell2mat(InfoPointsNearDetectedSoilSlips{i1,4}(:,7));
         PointsNearAspect  = PointsNearAspect(IndexPointsInPolWin);
     
-        PlotAspect = fastscatter2(PointsNearLong, PointsNearLat, PointsNearAspect, 'FaceAlpha',0.4, 'Parent',ax_ind4);
+        PlotAspect = fastscatter2(PointsNearLong, PointsNearLat, PointsNearAspect, 'FaceAlpha',TTrans, 'Parent',ax_ind4);
     
         % cmap = summer(8);
         cmap = hsv(8);
@@ -474,5 +664,6 @@ for i1 = 1:length(PolWindow)
         close(fig_aspect)
     end
 end
+close(ProgressBar) % Fig instead of ProgressBar if in Standalone Version
 cd(fold0)
 warning('on')
