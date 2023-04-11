1e1749640d697e7b1e22410ead93a1487e8a364b
Salvatore Misiano <104531745+SalvatoreMisia@users.noreply.github.com>
2023-04-11 09:00:46 +0200

    feat: daily interpolation of different time sensitive data type

--- APP/MODULES/PostUserE_Step2_InterpolationDaily.m
+++ APP/MODULES/PostUserE_Step2_InterpolationDaily.m
@@ -1,32 +1,44 @@
-Fig = uifigure; % Remember to comment this line if is app version
+% Fig = uifigure; % Remember to comment this line if is app version
 ProgressBar = uiprogressdlg(Fig, 'Title','Please wait', 'Message','Reading files...', ...
                                  'Indeterminate','on');
 drawnow
 
 %% Loading data
 ProgressBar.Message = "Loading data...";
+Options = {'Rainfall', 'Temperature'};
+DataRead = uiconfirm(Fig, 'What type of data do you want to read?', ...
+                          'Reading Data', 'Options',Options, 'DefaultOption',2);
+switch DataRead
+    case 'Rainfall'
+        GeneralFileName = 'GeneralRainfall.mat';
+        ShortName       = 'Rain';
+    case 'Temperature'
+        GeneralFileName = 'GeneralTemperature.mat';
+        ShortName       = 'Temp';
+end
+
 cd(fold_var)
-load('GeneralRainfall.mat', 'GeneralRainData','RainGauges','RainfallDates') % Remember that RainfallDates are referred at the end of your registration period
+load(GeneralFileName,       'GeneralData','Gauges','RecDates') % Remember that RainfallDates are referred at the end of your registration period
 load('GridCoordinates.mat', 'IndexDTMPointsInsideStudyArea','xLongAll','yLatAll')
 cd(fold0)
 
 %% Elaboration of data and selection of dates
 ProgressBar.Message = "Selection od dates...";
-[xLongSta, yLatSta] = deal(RainGauges{2}(:,1), RainGauges{2}(:,2));
+[xLongSta, yLatSta] = deal(Gauges{2}(:,1), Gauges{2}(:,2));
 
 EndDateInd = listdlg('PromptString',{'Select the date of your event:',''}, ...
-                     'ListString',RainfallDates, 'SelectionMode','single');
+                     'ListString',RecDates, 'SelectionMode','single');
 
-MaxDaysPossible = caldays(between(RainfallDates(1), RainfallDates(EndDateInd), 'days'));
+MaxDaysPossible = caldays(between(RecDates(1), RecDates(EndDateInd), 'days'));
 
 NumberOfDays = str2double(inputdlg({["How many days do you want to consider? "
                                      strcat("(Max possible with your dataset:  ",string(MaxDaysPossible)," days")]}, ...
                                      '', 1, {num2str(MaxDaysPossible)}));
 
-EndDate          = RainfallDates(EndDateInd);
-StartDate        = RainfallDates(EndDateInd)-days(NumberOfDays);
-StartDateInd     = find(abs(minutes(RainfallDates-StartDate)) <= 1);
-dTRecordings     = RainfallDates(2)-RainfallDates(1);
+EndDate          = RecDates(EndDateInd);
+StartDate        = RecDates(EndDateInd)-days(NumberOfDays);
+StartDateInd     = find(abs(minutes(RecDates-StartDate)) <= 1);
+dTRecordings     = RecDates(2)-RecDates(1);
 StepForEntireDay = int64(days(1)/dTRecordings);
 
 if (NumberOfDays > MaxDaysPossible) || (NumberOfDays <= 0)
@@ -35,33 +47,56 @@ end
 
 %% Interpolation
 ProgressBar.Message = "Interpolation...";
-RainCumDay = zeros(size(GeneralRainData,1), NumberOfDays);
+DataDaily = zeros(size(GeneralData,1), NumberOfDays);
 for i1 = 1:NumberOfDays
-    RainCumDay(:,i1) = sum( GeneralRainData(:, (StartDateInd+StepForEntireDay*(i1-1)+1):StartDateInd+StepForEntireDay*(i1)), 2 );
+    switch DataRead
+        case 'Rainfall'
+            DataDaily(:,i1) = sum( GeneralData(:, (StartDateInd+StepForEntireDay*(i1-1)+1) : StartDateInd+StepForEntireDay*(i1)), 2 );
+        case 'Temperature'
+            DataDaily(:,i1) = mean( GeneralData(:, (StartDateInd+StepForEntireDay*(i1-1)+1) : StartDateInd+StepForEntireDay*(i1)), 2 );
+    end
 end
 
+Options = {'linear', 'nearest', 'natural'};
+InterpMethod = uiconfirm(Fig, 'What interpolation method do you want to use?', ...
+                              'Interpolation methods', 'Options',Options);
 ProgressBar.Indeterminate = 'off';
-RainInterpolated = cell(size(RainCumDay,2), size(xLongAll,2));
-for i1 = 1:size(RainCumDay,2)
-    ProgressBar.Value = i1/size(RainCumDay,2);
-    ProgressBar.Message = strcat("Interpolating day ", string(i1)," of ", string(size(RainCumDay,2)));
+DataInterpolated = cell(size(DataDaily,2), size(xLongAll,2));
+for i1 = 1:size(DataDaily,2)
+    ProgressBar.Value = i1/size(DataDaily,2);
+    ProgressBar.Message = strcat("Interpolating day ", string(i1)," of ", string(size(DataDaily,2)));
 
-    CurrInterpolation = scatteredInterpolant(xLongSta, yLatSta, RainCumDay(:,i1), 'natural');   
+    CurrInterpolation = scatteredInterpolant(xLongSta, yLatSta, DataDaily(:,i1), InterpMethod); 
     for i2 = 1:size(xLongAll,2)
         xLong = xLongAll{i2}(IndexDTMPointsInsideStudyArea{i2});
         yLat  = yLatAll{i2}(IndexDTMPointsInsideStudyArea{i2});
-        hw1   = max(CurrInterpolation(xLong,yLat), 0); % Rain evaluation only in the internal points 
-        RainInterpolated{i1,i2} = sparse(hw1); % On rows are saved temporal events of the same raster box
+        switch DataRead
+            case 'Rainfall'
+                TempValues = max(CurrInterpolation(xLong,yLat), 0);
+            case 'Temperature'
+                TempValues = CurrInterpolation(xLong,yLat);
+        end
+        DataInterpolated{i1,i2} = sparse(TempValues); % On rows are saved temporal events of the same raster box
     end      
 end
 ProgressBar.Indeterminate = 'on';
 
-DateInterpolationStarts = RainfallDates(StartDateInd):days(1):RainfallDates(StartDateInd)+days(NumberOfDays-1); % -1 because normally you would take 00:00 of the day after the desired, for having the complete interpolation of the past day.
+DateInterpolationStarts = RecDates(StartDateInd):days(1):RecDates(StartDateInd)+days(NumberOfDays-1); % -1 because normally you would take 00:00 of the day after the desired, for having the complete interpolation of the past day.
 
 %% Saving...
 ProgressBar.Message = "Saving...";
 cd(fold_var)
-save('RainInterpolated.mat',  'RainInterpolated','DateInterpolationStarts');
-save('DateInterpolation.mat', 'DateInterpolationStarts','StartDate','EndDate','RainCumDay');
+eval([ShortName,'Interpolated = DataInterpolated;'])
+clear('DataInterpolated')
+eval([ShortName,'DateInterpolationStarts = DateInterpolationStarts;'])
+clear('DateInterpolationStarts')
+eval([ShortName,'CumDay = DataDaily;'])
+clear('DataDaily')
+
+VariablesInterpolated = {[ShortName,'Interpolated'], [ShortName,'DateInterpolationStarts']};
+VariablesDates        = {[ShortName,'DateInterpolationStarts'], 'StartDate', 'EndDate', [ShortName,'CumDay']};
+
+save([ShortName,'Interpolated.mat']     , VariablesInterpolated{:});
+save([ShortName,'DateInterpolation.mat'], VariablesDates{:});
 cd(fold0)
 close(Fig)
