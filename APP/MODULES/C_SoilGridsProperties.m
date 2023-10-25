a84e4ae1c2d9a07ca4702c752caa70fcb216e437
SalvatoreMisia <104531745+SalvatoreMisia@users.noreply.github.com>
2023-10-25 09:26:39 +0200

    refactor: add the maps of vegetation probabilities

--- APP/MODULES/C_SoilGridsProperties.m
+++ APP/MODULES/C_SoilGridsProperties.m
@@ -1,11 +1,10 @@
-% Fig = uifigure; % Remember to comment this line if is app version
+if not(exist('Fig', 'var')); Fig = uifigure; end
 ProgressBar = uiprogressdlg(Fig, 'Title','Please wait', 'Message','Reading files...', 'Indeterminate','on');
 drawnow
 
 %% File loading...
-cd(fold_var)
-load('GridCoordinates.mat', 'xLongAll','yLatAll')
-cd(fold0)
+sl = filesep;
+load([fold_var,sl,'GridCoordinates.mat'], 'xLongAll','yLatAll')
 
 %% Properties extraction
 Options = {'linear', 'nearest', 'natural'};
@@ -30,8 +29,26 @@ WaitAns3 = uiconfirm(Fig, 'Now you will search for NDVI raster file!', 'Search f
 FullNameNDVI = [FilePathNDVI,FileNameNDVI];
 NdviAll = raster_interpolation(xLongAll, yLatAll, FullNameNDVI, InterpMethod);
 
-FullNamesToCopy = {FullNameClay, FullNameSand, FullNameNDVI};
-NamesOfRaseters = {FileNameClay, FileNameSand, FileNameNDVI};
+ProgressBar.Message = 'Vegetation probabilities values interpolation...';
+WaitAns4 = uiconfirm(Fig, 'Now you will search for vegetation probability files!', 'Search file', 'Options',{'Ok, come on!'});
+[FileNameVgPr, FilePathVgPr] = uigetfile('*.tif', 'Choose your rasters for vegetation probabilities', 'MultiSelect','on');
+FullNameVgPr = strcat(FilePathVgPr,FileNameVgPr);
+VgPrAll = cell(numel(FileNameVgPr), numel(xLongAll));
+for i1 = 1:length(FileNameVgPr)
+    VgPrAll(i1,:) = raster_interpolation(xLongAll, yLatAll, FullNameVgPr{i1}, InterpMethod);
+end
+
+RowToDel = any(cellfun(@isempty, VgPrAll), 2);
+FileNameVgPr(RowToDel) = [];
+FilePathVgPr(RowToDel) = [];
+FullNameVgPr(RowToDel) = [];
+VgPrAll(RowToDel, :)   = [];
+
+RowNames = inputdlg2(FileNameVgPr);
+VgPrAll  = array2table(VgPrAll, 'RowNames',RowNames);
+
+FullNamesToCopy = [{FullNameClay, FullNameSand, FullNameNDVI}, FullNameVgPr];
+NamesOfRaseters = [{FileNameClay, FileNameSand, FileNameNDVI}, FileNameVgPr];
 
 %% Creation of a new folder
 fold_raw_sg_rast = [fold_raw,sl,'SoilGrids Rasters'];
@@ -48,10 +65,9 @@ end
 
 %% Saving...
 ProgressBar.Message = 'Saving...';
-cd(fold_var)
-VariablesSoilGrids = {'ClayContentAll', 'SandContentAll', 'NdviAll', 'NamesOfRaseters'};
-save('SoilGrids.mat', VariablesSoilGrids{:})
-cd(fold0)
-save('os_folders.mat', 'fold_raw_sg_rast', '-append')
+
+VariablesSoilGrids = {'ClayContentAll', 'SandContentAll', 'NdviAll', 'VgPrAll', 'NamesOfRaseters'};
+saveswitch([fold_var,sl,'SoilGrids.mat'], VariablesSoilGrids{:})
+save([fold0,sl,'os_folders.mat'], 'fold_raw_sg_rast', '-append')
 
 close(ProgressBar)
