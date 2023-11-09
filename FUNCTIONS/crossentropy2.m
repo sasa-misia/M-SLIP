e09e800931eb42c1ecfa64e8a0ad539f2b2a79e6
Salvatore Misiano <salvatoremisiano96@gmail.com>
2023-11-09 16:12:52 +0100

    fix: resolve the loss evaluation problems (same names on 2 libraries)

--- FUNCTIONS/crossentropy2.m
+++ FUNCTIONS/crossentropy2.m
@@ -7,6 +7,27 @@ function CrssEntr = crossentropy2(Predictions, Targets)
 %
 %     CrssEntr = crossentropy2(Predictions, Targets)
 
-CrssEntr = sum(Targets.*log(Predictions))/numel(Predictions);
+% REMEMBER TO CHECK IT WITH MULTICLASS (MULTIOUTPUT AND PREDICTIONS)
+ClssUnq  = unique(Targets);
+ExpTrue  = arrayfun(@(x) double(Targets==x), ClssUnq, 'UniformOutput',false);
+
+RelPred  = cell(size(ExpTrue));
+for i1 = 1:length(RelPred)
+    if i1 == 1
+        RelPred{i1} = 1-sum(Predictions,2);
+    else
+        RelPred{i1} = Predictions(:,i1-1);
+    end
+end
+
+% In this section you search for values too small that will give nan with
+% the log. You just set a number small but not too.
+MinVal = 1e-15;
+IndToChng = cellfun(@(x) x<MinVal, RelPred, 'UniformOutput',false);
+for i1 = 1:numel(IndToChng)
+    RelPred{i1}(IndToChng{i1}) = MinVal;
+end
+
+CrssEntr = -sum(cellfun(@(x,y) sum(x.*log10(y)), ExpTrue, RelPred)) / numel(Targets);
 
 end
