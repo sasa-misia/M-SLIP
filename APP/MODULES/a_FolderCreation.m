5743634708e7d5662b66773a13b7c8a0d1a926ef
SalvatoreMisia <104531745+SalvatoreMisia@users.noreply.github.com>
2024-01-09 14:14:57 +0100

    feat: add check of toolboxes

--- APP/MODULES/a_FolderCreation.m
+++ APP/MODULES/a_FolderCreation.m
@@ -1,4 +1,4 @@
-%% OS identification
+%% Os identification
 if ispc
     sl = '\';
     user = string(getenv('username'));
@@ -8,6 +8,28 @@ elseif ismac
 else
     disp('Platform not supported')
     error('PNS')
+end
+
+%% Check of toolboxes
+MATLABVer = ver;
+InstTools = {MATLABVer.Name};
+ReqTools  = {'Image Processing Toolbox', 'Mapping Toolbox', 'Upslope Area Toolbox'};
+OptTools  = {'Curve Fitting Toolbox', 'Deep Learning HDL Toolbox', 'Deep Learning Toolbox', ...
+             'Lidar Toolbox', 'NCTOOLBOX Tools for read-only access to Common Data Model dataset', ...
+             'Optimization Toolbox', 'Statistics and Machine Learning Toolbox'};
+
+for i1 = 1:numel(ReqTools)
+    CheckReq = any(strcmp(ReqTools{i1}, InstTools));
+    if not(CheckReq)
+        error(['You must install this Toolbox: ',ReqTools{i1}])
+    end
+end
+
+for i1 = 1:numel(OptTools)
+    CheckOpt = any(strcmp(OptTools{i1}, InstTools));
+    if not(CheckOpt)
+        warning(['You should install this Toolbox: ',OptTools{i1}])
+    end
 end
 
 %% Removing existing variable 'folders' to create the new one
